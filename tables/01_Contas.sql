9d85da6165ccf849876c6d72fd5bcb0207d8c324
antoniomribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-07 17:53:47 +0100

    Alterei 'Tipo' de ENUM para VARCHAR
    Foi a única maneira que consegui para que depois a FK na tabela 'Lancamentos' funcionasse

--- tables/01_Contas.sql
+++ tables/01_Contas.sql
@@ -1,10 +1,10 @@
 -- Tabela: Contas
 -- Descrição: Lista todos os nomes e propriedades das contas contabilísticas da aplicação
 
-CREATE TABLE Contas(
+CREATE TABLE Contas (
   Conta VARCHAR(20) NOT NULL,
   Nome VARCHAR(255) NOT NULL,
-  Tipo ENUM('CentroResultados','Analitica','Fornecedor','Cliente','Colaborador'),
+  Tipo VARCHAR(50) NOT NULL,
   Extra JSON,
-  PRIMARY KEY(Conta,Tipo)
+  PRIMARY KEY (Conta,Tipo)
 );
