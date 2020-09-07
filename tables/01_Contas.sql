708e465677335772e3cd359e298534c206c346f1
antoniomribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-07 16:33:49 +0100

    Alterar 'Conta' de INT para VARCHAR

--- tables/01_Contas.sql
+++ tables/01_Contas.sql
@@ -2,7 +2,7 @@
 -- Descrição: Lista todos os nomes e propriedades das contas contabilísticas da aplicação
 
 CREATE TABLE Contas(
-  Conta INT,
+  Conta VARCHAR(20) NOT NULL,
   Nome VARCHAR(255) NOT NULL,
   Tipo ENUM('CentroResultados','Analitica','Fornecedor','Cliente','Colaborador'),
   Extra JSON,
