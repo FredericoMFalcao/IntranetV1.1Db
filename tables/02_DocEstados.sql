6744f904bbd1299dcc3185b85c5dea1822426ba1
antoniomribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-07 12:38:15 +0100

    Create 02_DocEstados.sql

---
+++ tables/02_DocEstados.sql
@@ -0,0 +1,10 @@
+-- Tabela: DocEstados
+-- Descricao: Lista todos os estados que cada tipo de documento pode ter
+
+CREATE TABLE DocEstados (
+  TipoDoc VARCHAR(50) NOT NULL,
+  Estado VARCHAR(50) NOT NULL,
+  Descricao VARCHAR(255),
+  
+  PRIMARY KEY (TipoDoc,Estado)
+);
