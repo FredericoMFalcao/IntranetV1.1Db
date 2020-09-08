1de850eed41ac1e8e663ef4f6c168453cf466b07
antoniomribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-08 15:56:26 +0100

    Pus FK composta ('Tipo' + 'EstadoFinal')

--- tables/40_DocWorkflow.sql
+++ tables/40_DocWorkflow.sql
@@ -3,12 +3,13 @@
 
 CREATE TABLE DocWorkflow (
   NumSerie VARCHAR(25) NOT NULL, -- foreign key
-  Tipo VARCHAR(50) NOT NULL,
+  Tipo VARCHAR(50) NOT NULL, -- foreign key
   EstadoInicial VARCHAR(50) NOT NULL,
-  EstadoFinal VARCHAR(50) NOT NULL,
+  EstadoFinal VARCHAR(50) NOT NULL, -- foreign key
   Utilizador VARCHAR(20) NOT NULL, -- foreign key
   DataHora DATETIME NOT NULL,
 
   FOREIGN KEY (NumSerie) REFERENCES Documentos(NumSerie) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Tipo,EstadoFinal) REFERENCES DocEstados(TipoDoc,Estado) ON DELETE RESTRICT ON UPDATE CASCADE,
   FOREIGN KEY (Utilizador) REFERENCES Utilizadores(Utilizador) ON DELETE RESTRICT ON UPDATE CASCADE
 );
