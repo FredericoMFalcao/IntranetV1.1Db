e8fae579fc5833a864099ea5512ab49822c28232
Frederico <FredericoMFalcao@hotmail.com>
2020-09-08 16:27:58 +0100

    Acrescentei dados iniciais
    Com todos os estados existentes hoje em dia na AFM.

--- tables/02_DocEstados.sql
+++ tables/02_DocEstados.sql
@@ -1,10 +1,18 @@
 -- Tabela: DocEstados
 -- Descricao: Lista todos os estados que cada tipo de documento pode ter
 
-CREATE TABLE DocEstados (
+CREATE TABLE DocEstados (  
   TipoDoc VARCHAR(50) NOT NULL,
   Estado VARCHAR(50) NOT NULL,
   Descricao VARCHAR(255),
   
   PRIMARY KEY (TipoDoc,Estado)
 );
+
+
+-- Dados Iniciais
+INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarFornecedor', 'Existe no sistema apenas com um PDF.');
+INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarAnalitica',  'À espera de ser classificada em termos de contabilidade analítica.');
+INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarPagamento',     'À espera de ser anexado comprovativo de pagamento.');
+INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarContabilidade', 'À espera de intregrar custo no software de contabilidade fiscal.');
+INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarPagamentoContabilidade', 'À espera de intregrar pagamento no software de contabilidade fiscal.');
