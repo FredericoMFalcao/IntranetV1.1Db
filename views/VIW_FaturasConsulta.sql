d8f461ea096edc60ec9289f98dfb98a0c601fe53
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-09 19:18:22 +0100

    Create VIW_FaturasConsulta.sql

---
+++ views/VIW_FaturasConsulta.sql
@@ -0,0 +1,24 @@
+-- View: VIW_FaturasConsulta
+-- Descrição: Lista todas as faturas para a window 'Estado das Faturas'
+-- Depende de: tabelas 'Documentos', 'Lancamentos' e 'Contas'
+
+DROP VIEW IF EXISTS VIW_FaturasConsulta;
+
+CREATE VIEW VIW_FaturasConsulta AS
+SELECT
+	a.NumSerie,
+	CAST(JSON_EXTRACT (a.Extra, '$.DataFatura'), DATE) AS DataFatura,
+	JSON_EXTRACT (a.Extra, '$.NumFatura') AS NumFatura,
+	c.Nome AS FornecedorNome,
+	JSON_EXTRACT (c.Extra, '$.FornecedorNIF') AS FornecedorNIF,
+	CAST(JSON_EXTRACT (a.Extra, '$.Valor'), DECIMAL(18,2)) AS Valor,
+	JSON_EXTRACT (a.Extra, '$.Moeda') AS Moeda,
+	JSON_EXTRACT (a.Extra, '$.Projecto') AS Projecto
+FROM Documentos AS a
+	JOIN Lancamentos AS b
+		ON a.NumSerie = b.DocNumSerie
+	JOIN Contas AS c
+		ON b.Conta = c.Conta AND b.TipoConta = c.Tipo
+WHERE a.Tipo = 'FacturaFornecedor'
+	AND c.TipoConta = 'Fornecedor'
+;
