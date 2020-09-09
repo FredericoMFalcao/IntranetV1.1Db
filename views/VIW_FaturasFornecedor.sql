b31478a340a07d87c323c21ffd5c5c56fc73c6d6
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-09 18:53:20 +0100

    Create VIW_FaturasFornecedor.sql
    Desdobramento do campo 'Extra' + informações sobre os fornecedores. Por enquanto ignora impostos e moedas.

---
+++ views/VIW_FaturasFornecedor.sql
@@ -0,0 +1,31 @@
+-- View: VIW_FaturasFornecedor
+-- Descrição: Lista todas as faturas de fornecedores e algumas propriedades
+-- Depende de: tabelas 'Documentos', 'Lancamentos' e 'Contas'
+
+DROP VIEW IF EXISTS VIW_FaturasFornecedor;
+
+CREATE VIEW VIW_FaturasFornecedor AS
+SELECT
+	a.NumSerie,
+	JSON_EXTRACT (a.Extra, '$.NumFatura') AS NumFatura,
+	JSON_EXTRACT (a.Extra, '$.Projecto') AS Projecto,
+	CAST(JSON_EXTRACT (a.Extra, '$.DataFatura'), DATE) AS DataFatura,
+	CAST(JSON_EXTRACT (a.Extra, '$.DataRecebida'), DATE) AS DataRecebida,
+	CAST(JSON_EXTRACT (a.Extra, '$.PeriodoFaturacaoInicio'), DATE) AS PeriodoFaturacaoInicio,
+	CAST(JSON_EXTRACT (a.Extra, '$.PeriodoFaturacaoFim'), DATE) AS PeriodoFaturacaoFim,
+	CAST(JSON_EXTRACT (a.Extra, '$.DataValFatura'), DATE) AS DataValFatura,
+	a.Estado,
+	c.Conta AS FornecedorCodigo,
+	c.Nome AS FornecedorNome,
+	JSON_EXTRACT (c.Extra, '$.FornecedorNIF') AS FornecedorNIF,
+	CAST(JSON_EXTRACT (a.Extra, '$.Valor'), DECIMAL(18,2)) AS Valor,
+	JSON_EXTRACT (a.Extra, '$.Moeda') AS Moeda,
+	JSON_EXTRACT (a.Extra, '$.Descricao') AS Descricao
+FROM Documentos AS a
+	JOIN Lancamentos AS b
+		ON a.NumSerie = b.DocNumSerie
+	JOIN Contas AS c
+		ON b.Conta = c.Conta AND b.TipoConta = c.Tipo
+WHERE a.Tipo = 'FacturaFornecedor'
+	AND c.TipoConta = 'Fornecedor'
+;
