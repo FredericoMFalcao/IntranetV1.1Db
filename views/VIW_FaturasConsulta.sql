5107ec652ded64f2f652d9a2a3e30897c9e0a127
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-10 08:10:06 +0100

    fixed typos

--- views/VIW_FaturasConsulta.sql
+++ views/VIW_FaturasConsulta.sql
@@ -13,12 +13,12 @@ SELECT
 	JSON_EXTRACT (c.Extra, '$.FornecedorNIF') AS FornecedorNIF,
 	CAST(JSON_EXTRACT (a.Extra, '$.Valor'), DECIMAL(18,2)) AS Valor,
 	JSON_EXTRACT (a.Extra, '$.Moeda') AS Moeda,
-	JSON_EXTRACT (a.Extra, '$.Projecto') AS Projecto
+	JSON_EXTRACT (a.Extra, '$.Projeto') AS Projeto
 FROM Documentos AS a
 	JOIN Lancamentos AS b
 		ON a.NumSerie = b.DocNumSerie
 	JOIN Contas AS c
 		ON b.Conta = c.Conta AND b.TipoConta = c.Tipo
-WHERE a.Tipo = 'FacturaFornecedor'
+WHERE a.Tipo = 'FaturaFornecedor'
 	AND c.TipoConta = 'Fornecedor'
 ;
