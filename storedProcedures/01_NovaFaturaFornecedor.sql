e83221e2b0bf6a2eff18b9668251ca180f43f989
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-10 14:57:45 +0100

    Acresentei validação argumentos

--- storedProcedures/01_NovaFaturaFornecedor.sql
+++ storedProcedures/01_NovaFaturaFornecedor.sql
@@ -2,13 +2,18 @@ DELIMITER //
 
 CREATE PROCEDURE NovaFaturaFornecedor (IN NumSerie TEXT, IN FileId TEXT )
  BEGIN
+ 
+  -- 0. Verificar validade dos argumentos
+  IF NumSerie NOT REGEXP '^FT[A-Z][a-z][0-9]{2}#[0-9]{3,4}\.pdf$'
+   THEN signal sqlstate '20000' set message_text = 'NumSerie com formato inválido';
+  END IF;
+   
   -- 0. Começar Transacao
   START TRANSACTION;
   
   -- 1. Inserir em Documentos 
   INSERT INTO Documentos (NumSerie, Tipo, Estado, FileId) 
   VALUES (NumSerie, 'FaturaFornecedor', 'PorClassificarFornecedor', FileId);
-  
   
   -- 10. Salvar
   COMMIT;
