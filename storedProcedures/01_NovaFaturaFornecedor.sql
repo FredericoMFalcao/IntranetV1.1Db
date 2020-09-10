094c78ae39b630f3a779796095f02107c93a5fe8
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-10 14:35:06 +0100

    Update 01_NovaFaturaFornecedor.sql

--- storedProcedures/01_NovaFaturaFornecedor.sql
+++ storedProcedures/01_NovaFaturaFornecedor.sql
@@ -2,9 +2,15 @@ DELIMITER //
 
 CREATE PROCEDURE NovaFaturaFornecedor (IN NumSerie TEXT, IN FileId TEXT )
  BEGIN
+  --- 0. Começar Transacao
   START TRANSACTION;
+  
+  -- 1. Inserir em Documentos 
   INSERT INTO Documentos (NumSerie, Tipo, Estado, FileId) 
-  VALUES (NumSerie, 'FaturaFornecedor', 'PorClassificarFornecedor', FileId) 
+  VALUES (NumSerie, 'FaturaFornecedor', 'PorClassificarFornecedor', FileId);
+  
+  
+  -- 10. Salvar
   COMMIT;
  END;
 //
