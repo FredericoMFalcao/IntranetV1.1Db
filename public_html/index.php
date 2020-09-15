8de76c557cfa398c3d84a993f9c2593da52583cf
Frederico <FredericoMFalcao@hotmail.com>
2020-09-15 13:25:43 +0100

    Changed labels for better readability

--- public_html/index.php
+++ public_html/index.php
@@ -5,13 +5,13 @@
 <h2>Links</h2>
 <ul>
 	<li>	<a href="sql_explorer.php">sql explorer</a></li>
-	<li>	<a href="deploy_code.php">update code</a></li>
+	<li>	<a href="deploy_code.php">force code deployment</a></li>
 </ul>
 <hr>
 <h2>Last WebServer Errors</h2>
 <pre><?php passthru("tail ../log/apache2_error.log");  ?></pre>
 <hr>
-<h2>Last Compilation Error</h2>
+<h2>Last Compilation Status</h2>
 <pre><?php echo file_get_contents("./last_update_error.txt"); ?></pre>
 <hr>
 <h2>Last Compilation Text</h2>
