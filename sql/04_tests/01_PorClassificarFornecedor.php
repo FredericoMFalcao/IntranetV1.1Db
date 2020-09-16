d7340ad8ce4361c3a32d61112c07fb491624811c
Frederico <FredericoMFalcao@hotmail.com>
2020-09-16 17:08:58 +0100

    Adicionei teste para classificação de fornecedor

--- sql/04_tests/01_PorClassificarFornecedor.php
+++ sql/04_tests/01_PorClassificarFornecedor.php
@@ -18,4 +18,12 @@ require_once __DIR__."/_tests_lib.php";
 	->expectQuery('CALL NovaFaturaFornecedor("GG19#001.pdf","xxx-xxx")')
 	->toErrWithCode("20000")
 )
+->addTest(
+	/* #3 : Classificar um documento com fornecedor */
+	(new UnitTest())
+	->describe("Classificar fatura de fornecedor com múltiplos campos")
+	->expectQuery('CALL FatFornecedor_ClassificarFornecedor("FTAn12#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
+	->toSucceed()
+)
+
 ->go();
