701d05b658ed6de00cc1f53e594c67dd88d1f8ab
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-17 10:50:14 +0100

    'Conta' passa a poder ser composta

--- sql/01_tables/41_Lancamentos.sql
+++ sql/01_tables/41_Lancamentos.sql
@@ -2,16 +2,11 @@
 -- Descricao: Regista todos os lançamentos contabilísticos da empresa
 
 CREATE TABLE Lancamentos (
-  Conta        VARCHAR(20) NOT NULL, -- foreign key
-  TipoConta    VARCHAR(50) NOT NULL, -- foreign key
-  
-  CoefRateio   FLOAT NOT NULL,
-  
-  Mes          DATE NOT NULL,        -- desc: primeiro dia do mês a que o valor diz respeito
+  Conta        VARCHAR(30) NOT NULL, -- pode ser conta composta (e.g. '0101:020403:ABC' para Centro Resultados, Analítica e Colaborador)
+  CoefRateio   FLOAT NOT NULL,       -- percentagem do valor total do respectivo documento (pode variar entre -1 e 1)
+  Mes          DATE NOT NULL,        -- primeiro dia do mês a que o valor diz respeito
   DocNumSerie  VARCHAR(25) NOT NULL, -- foreign key
-  Extra        JSON,
   
-  FOREIGN KEY (Conta,TipoConta) REFERENCES Contas(Conta,Tipo) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (Conta,CoefRateio,Mes,DocNumSerie),
   FOREIGN KEY (DocNumSerie) REFERENCES Documentos(NumSerie) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
