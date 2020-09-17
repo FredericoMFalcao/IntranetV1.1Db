5df5948af96636fadf4505470065ba0cbbf3e38e
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-17 13:23:19 +0100

    Update and rename 02_FatFornecedor_ClassificarFornecedor.sql to 02_FF_ClassificarFornecedor.sql
    Passa a fazer dois lançamentos de sentido contrário (um na conta de fornecedores e outro numa conta de custos gerais).
    Passa a verificar se o NumSerie de input já existe na tabela 'Documentos'.
    O estado avança para 'PorClassificarAnalitica'.

--- sql/03_storedProcedures/02_FF_ClassificarFornecedor.sql
+++ sql/03_storedProcedures/02_FF_ClassificarFornecedor.sql
@@ -1,8 +1,8 @@
-DROP PROCEDURE IF EXISTS FatFornecedor_ClassificarFornecedor;
+DROP PROCEDURE IF EXISTS FF_ClassificarFornecedor;
 
 DELIMITER //
 
-CREATE PROCEDURE FatFornecedor_ClassificarFornecedor (
+CREATE PROCEDURE FF_ClassificarFornecedor (
 IN NumSerie                 TEXT,
 IN NumFatura                TEXT,
 IN Projecto                 TEXT,
@@ -11,33 +11,35 @@ IN DataRecebida             DATE,
 IN PeriodoFacturacao        TEXT,
 -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
 IN DataValFactura           TEXT,
-
 IN FornecedorCodigo         TEXT,
-
 IN Valor                    TEXT,
 -- e.g. {"Bens": {"ValorBase": 0.00, "Iva": 0.00}, "Servicos": {"ValorBase":0.00,"Iva":0.00}}
-
 IN Moeda                    TEXT,
-
 IN Descricao                TEXT
 )
  BEGIN
  
   -- 0. Verificar validade dos argumentos
- 
+  IF NumSerie NOT IN (SELECT NumSerie FROM Documentos)
+   THEN signal sqlstate '20000' set message_text = 'Não existe nenhuma factura com este NumSerie';
+  END IF;
    
   -- 1. Começar Transacao
   START TRANSACTION;
   
   -- 2. Alterar dados
-  -- 2.1 Inserir Lancamento Fornecedor
-  INSERT INTO Lancamentos (Conta, TipoConta, CoefRateio, DocNumSerie) 
-  VALUES (FornecedorCodigo, 'Fornecedor', 1, NumSerie);
+  -- 2.1 Inserir Lançamento Fornecedor
+  INSERT INTO Lancamentos (Conta, CoefRateio, Mes, DocNumSerie)
+  VALUES (FornecedorCodigo, 1, DataFatura, NumSerie);
   
-  -- 2.2 Acrescentar dados a documento
+  -- 2.2 Inserir Lançamento Custos Gerais
+  INSERT INTO Lancamentos (Conta, CoefRateio, Mes, DocNumSerie)
+  VALUES ("CG01", -1, DataFatura, NumSerie);
+  
+  -- 2.3 Acrescentar dados a documento
   UPDATE Documentos
    SET
-    Estado = 'PorClassificarFornecedor',
+    Estado = 'PorClassificarAnalitica',
     Extra = JSON_SET(Extra, 
         '$.NumFatura', NumFatura,
         '$.Projeto', Projeto,
