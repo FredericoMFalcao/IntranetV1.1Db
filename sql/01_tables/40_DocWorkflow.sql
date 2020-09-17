b07730fa8551fd04c1f0c3271fd5eb3a7aa8084e
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-17 10:09:40 +0100

    Passa a registar info antes/depois em JSON

--- sql/01_tables/40_DocWorkflow.sql
+++ sql/01_tables/40_DocWorkflow.sql
@@ -3,13 +3,14 @@
 
 CREATE TABLE DocWorkflow (
   NumSerie VARCHAR(25) NOT NULL, -- foreign key
-  Tipo VARCHAR(50) NOT NULL, -- foreign key
-  EstadoInicial VARCHAR(50) NOT NULL,
-  EstadoFinal VARCHAR(50) NOT NULL, -- foreign key
+  TipoDoc VARCHAR(50) NOT NULL, -- foreign key
+  Descricao VARCHAR(250) NOT NULL,
   Utilizador VARCHAR(20) NOT NULL, -- foreign key
   DataHora DATETIME NOT NULL,
+  DocInfoAntes JSON NOT NULL,
+  DocInfoDepois JSON NOT NULL
 
   FOREIGN KEY (NumSerie) REFERENCES Documentos(NumSerie) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (Tipo,EstadoFinal) REFERENCES DocEstados(TipoDoc,Estado) ON DELETE RESTRICT ON UPDATE CASCADE
+  FOREIGN KEY (TipoDoc) REFERENCES DocTipos(Tipo) ON DELETE RESTRICT ON UPDATE CASCADE
 --  FOREIGN KEY (Utilizador) REFERENCES Utilizadores(Utilizador) ON DELETE RESTRICT ON UPDATE CASCADE
 );
