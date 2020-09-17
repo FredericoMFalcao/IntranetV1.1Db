def25765e04752e6f46d25c55c87957dfbc3bd46
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-17 14:52:06 +0100

    Update and rename 03_FatFornecedor_ClassificarAnalitica.sql to 03_FF_ClassificarAnalitica.sql
    Passa a fazer lançamentos de sentido contrário (um ou mais nas contas de analítica e outro numa conta de custos gerais).
    Passa a verificar se o NumSerie de input já existe na tabela 'Documentos'.
    O estado avança para 'PorAnexarCPagamento'.

---
+++ sql/03_storedProcedures/03_FF_ClassificarAnalitica.sql
@@ -0,0 +1,55 @@
+DROP PROCEDURE IF EXISTS FF_ClassificarAnalitica;
+
+DELIMITER //
+
+CREATE PROCEDURE FF_ClassificarAnalitica (
+IN NumSerie                      TEXT,
+IN ValorFatura                   DECIMAL(18,2),
+IN ClassificacaoAnalitica        TEXT
+-- e.g. [{"CentroResultados": "0101", "Analitica": "0202", "Colaborador": "ABC", "Valor": 1000}, {...}]
+)
+ BEGIN
+  DECLARE i INT;
+  SET i = 0;
+ 
+  -- 0. Verificar validade dos argumentos
+  IF NumSerie NOT IN (SELECT NumSerie FROM Documentos)
+   THEN signal sqlstate '20000' set message_text = 'Não existe factura com este NumSerie';
+  END IF;
+   
+  -- 1. Começar Transacao
+  START TRANSACTION;
+  
+  -- 2. Alterar dados
+  -- 2.1 Inserir lançamentos com analíticas discriminadas
+  WHILE i != JSON_LENGTH(ClassificacaoAnalitica) DO
+ 
+   INSERT INTO Lancamentos (Conta, CoefRateio, Mes, DocNumSerie)
+    VALUES (
+     CONCAT_WS(":",
+      JSON_EXTRACT(JSON_EXTRACT(ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.CentroResultados'),
+      JSON_EXTRACT(JSON_EXTRACT(ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Analitica'),
+      JSON_EXTRACT(JSON_EXTRACT(ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Colaborador')
+     ),
+     JSON_EXTRACT(JSON_EXTRACT(ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Valor') / ValorFatura * -1,
+     NumSerie
+    );
+
+   SET i = i + 1;
+   
+  END WHILE;
+
+  -- 2.2 Inserir lançamento em custos gerais (com sinal contrário ao que foi lançado ao classificar o fornecedor)
+   INSERT INTO Lancamentos (Conta, CoefRateio, Mes, DocNumSerie)
+    VALUES ("CG01", 1, DataFatura, NumSerie);
+
+  -- 2.3 Alterar estado do documento
+   UPDATE Documentos
+   SET Estado = 'PorAnexarCPagamento',
+                  
+  -- 10. Salvar
+  COMMIT;
+ END;
+//
+
+DELIMITER ;
