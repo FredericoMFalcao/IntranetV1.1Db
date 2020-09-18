55673baa6f74ee5f694bc065d5e6c4ece2b17e1b
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-09-18 18:39:39 +0000

    created a base for a javascript web app

--- public_html/index.php
+++ public_html/index.php
@@ -1,22 +1,24 @@
 <html>
 <head>
+
+<script>
+<?php 
+      require_once __DIR__."/../db_support_functions.php";
+      if(($js_funcs = sql("SELECT Name, InputArgs_json, Code  FROM PLT_GUI_Javascript"))) 
+	foreach($js_funcs as $js_func) {
+		extract($js_func);
+		echo "\bfunction ".$Name."(".implode(", ",array_values($InputArgs_json)).") {\n";
+		echo $Code;
+		echo "\n}\n";
+	}
+?>
+</script>
 </head>
 <body>
-<h2>Links</h2>
-<ul>
-	<li>	<a href="sql_explorer.php">sql explorer</a></li>
-	<li>	<a href="deploy_code.sh">force code deployment</a></li>
-</ul>
-<hr>
-<h2>Last WebServer Errors</h2>
-<pre><?php passthru("tail ../log/apache2_error.log");  ?></pre>
-<hr>
-<h2>Last Compilation Status</h2>
-<pre><?php echo file_get_contents("./last_update_error.txt"); ?></pre>
-<hr>
-<h2>Last Compilation Text</h2>
-<pre><?php echo file_get_contents("./last_compiled_code.txt"); ?></pre>
-<h2>Last Unit Test Results</h2>
-<pre><?php echo file_get_contents("./last_unit_test_results.html"); ?></pre>
+
+<script>
+	var rootController = RootController();
+	document.body.append(rootController.renderDOM())
+</script>
 </body>
 </html>
