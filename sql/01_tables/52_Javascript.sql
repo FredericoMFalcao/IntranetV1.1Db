cd15f60461afc2bfdf7fc4edf47a9b73dca7398a
Frederico <FredericoMFalcao@hotmail.com>
2020-09-18 20:04:24 +0100

    Adjust identation

--- sql/01_tables/52_Javascript.sql
+++ sql/01_tables/52_Javascript.sql
@@ -3,13 +3,13 @@
 --              (1) The Code includes is ONLY the javascript code inside the function, not the wrapper "function xxx (...) { ... }"
 
 CREATE TABLE PLT_GUI_Javascript (
- _id 			      int(11) NOT NULL AUTO_INCREMENT,
- lastUpdate 	  timestamp NOT NULL DEFAULT current_timestamp(),
- Namespace 		  varchar(255) DEFAULT NULL,
- FuncName 		  varchar(255) NOT NULL,
+ _id 			        int(11) NOT NULL AUTO_INCREMENT,
+ lastUpdate 	   timestamp NOT NULL DEFAULT current_timestamp(),
+ Namespace 		   varchar(255) DEFAULT NULL,
+ FuncName 		    varchar(255) NOT NULL,
  InputArgs_json longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
  Description 	  varchar(255) DEFAULT NULL,
- Code 			    text NOT NULL,
+ Code 			       text NOT NULL,
  PRIMARY KEY (_id),
  UNIQUE KEY Namespace (Namespace,FuncName)
 );
