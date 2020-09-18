c52571ee76de1e3dd413e0fb4e6b9b4490778dc8
Frederico <FredericoMFalcao@hotmail.com>
2020-09-18 12:30:18 +0100

    fixed typo

--- sql/04_tests/01_PorClassificarFornecedor.php
+++ sql/04_tests/01_PorClassificarFornecedor.php
@@ -22,7 +22,7 @@ require_once __DIR__."/_tests_lib.php";
 	/* #3 : Classificar um documento com fornecedor */
 	(new UnitTest())
 	->describe("Classificar fatura de fornecedor com múltiplos campos")
-	->expectQuery('CALL FF_ClassificarFornecedor("FTAn12#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
+	->expectQuery('CALL FF_ClassificarFornecedor("FTAn19#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
 	->toSucceed()
 )
 
