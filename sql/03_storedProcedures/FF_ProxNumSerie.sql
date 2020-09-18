adde26fe46c295a5fec6457797ca9273311123bc
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-18 12:44:17 +0100

    Create FF_ProxNumSerie.sql

---
+++ sql/03_storedProcedures/FF_ProxNumSerie.sql
@@ -0,0 +1,23 @@
+DROP FUNCTION IF EXISTS FF_ProxNumSerie;
+
+DELIMITER //
+
+CREATE FUNCTION FF_ProxNumSerie (Pais TEXT, Ano TEXT) -- e.g. Pais = 'An', Ano = '20'
+RETURNS TEXT
+RETURN
+ CONCAT('FT',
+  Pais,
+  Ano,
+  '#',
+  (
+   SELECT MAX(SUBSTRING_INDEX(SUBSTRING_INDEX(NumSerie,".",1),"#",-1)) + 1
+   FROM Documentos
+   WHERE Tipo = 'FaturaFornecedor'
+    AND LEFT(NumSerie,6) = CONCAT('FT', Pais, Ano)
+  ),
+  '.pdf'
+ ),
+ 
+END //
+
+DELIMITER ;
