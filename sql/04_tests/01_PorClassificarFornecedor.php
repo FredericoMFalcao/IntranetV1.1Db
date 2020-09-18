ad049fc3964f88fb7ef9c517025f938df43d5835
Frederico <FredericoMFalcao@hotmail.com>
2020-09-18 12:29:25 +0100

    Atualizei nomes dos s.procedures

--- sql/04_tests/01_PorClassificarFornecedor.php
+++ sql/04_tests/01_PorClassificarFornecedor.php
@@ -8,21 +8,21 @@ require_once __DIR__."/_tests_lib.php";
 	/* #1 : Formato do número de série */
 	(new UnitTest())
 	->describe("Classificar fatura de fornecedor com num de série válido (FTAn19#001.pdf).")
-	->expectQuery('CALL NovaFaturaFornecedor("FTAn19#001.pdf","xxx-xxx")')
+	->expectQuery('CALL FF_NovaFatura("FTAn19#001.pdf","xxx-xxx")')
 	->toSucceed()
 )
 ->addTest(
 	/* #2 : Formato do número de série */
 	(new UnitTest())
-	->describe("Bloquear fatura de fornecedor com num de série inválido.")
-	->expectQuery('CALL NovaFaturaFornecedor("GG19#001.pdf","xxx-xxx")')
+	->describe("Impedir fatura de fornecedor com num de série inválido.")
+	->expectQuery('CALL FF_NovaFatura("GG19#001.pdf","xxx-xxx")')
 	->toErrWithCode("20000")
 )
 ->addTest(
 	/* #3 : Classificar um documento com fornecedor */
 	(new UnitTest())
 	->describe("Classificar fatura de fornecedor com múltiplos campos")
-	->expectQuery('CALL FatFornecedor_ClassificarFornecedor("FTAn12#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
+	->expectQuery('CALL FF_ClassificarFornecedor("FTAn12#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
 	->toSucceed()
 )
 
