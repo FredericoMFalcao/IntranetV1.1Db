32d6c5d6f9960d545f6a19818cd00e58e7b70913
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-09-22 14:44:31 +0000

    allows javascript functions to have optional arguments

--- 02_gui/RootController.js
+++ 02_gui/RootController.js
@@ -1,6 +1,6 @@
 // @Description: this is the main controller of the application
-// @param simpleString1 string this a a simple test string one
-// @param simpleString2 string this a a simple test string two
+// @param simpleString1 string? this a a simple test string one
+// @param simpleString2 string? this a a simple test string two
 // @return string
 // function RootController() {
 return {
--- public_html/index.php
+++ public_html/index.php
@@ -8,7 +8,17 @@
       if(($js_funcs = sql("SELECT FuncName, InputArgs_json, Code  FROM PLT_GUI_Javascript")) !== false)  
 	foreach($js_funcs as $js_func) {
 		extract($js_func);
-		echo "\nfunction ".$FuncName."(".implode(", ",array_values($InputArgs_json)).") {\n";
+		echo "\nfunction ".$FuncName."(".implode(", ",array_keys($InputArgs_json)).") {\n";
+		/* Add runtime typecheck and errors */
+		echo "/* Check type validity */\n";
+		foreach($InputArgs_json as $name => $type) {
+			if (strpos($type, "?") !== false) {$optional = true; $type = str_replace("?","",$type);} else $optional = false;
+			echo "if (";
+			echo "typeof $name != \"$type\" ";
+			if ($optional) echo " && typeof $name != \"undefined\"";
+			echo ") ";
+			echo "throw \"Invalid type for $name. Expected $type got \"+typeof $name;\n";
+		}
 		echo $Code;
 		echo "\n}\n";
 	}
