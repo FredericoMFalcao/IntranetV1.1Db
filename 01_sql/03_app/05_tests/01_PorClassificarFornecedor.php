57a7af2c86a1b5c3fc672a877ed38b9ccaeb6974
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-25 18:21:35 +0100

    acrescentei teste para nova fatura

--- 01_sql/03_app/05_tests/01_PorClassificarFornecedor.php
+++ 01_sql/03_app/05_tests/01_PorClassificarFornecedor.php
@@ -5,32 +5,22 @@ require_once __DIR__."/_tests_lib.php";
 
 (new TestSuite("Faturas Fornecedor"))
 ->addTest(
-	/* #1 : Formato do número de série */
 	(new UnitTest())
-	->describe("Classificar fatura de fornecedor com num de série válido (FTAn19#001.pdf).")
-	->expectQuery('CALL FF_NovaFatura("FTAn19#001.pdf","xxx-xxx")')
-	->toSucceed()
-)
-->addTest(
-	/* #2 : Formato do número de série */
-	(new UnitTest())
-	->describe("Impedir fatura de fornecedor com num de série inválido.")
-	->expectQuery('CALL FF_NovaFatura("GG19#001.pdf","xxx-xxx")')
-	->toErrWithCode("20000")
-)
-->addTest(
-	/* #3 : Classificar um documento com fornecedor */
-	(new UnitTest())
-	->describe("Classificar fatura de fornecedor com múltiplos campos")
-	->expectQuery('CALL FF_ClassificarFornecedor("FTAn19#001.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
-	->toSucceed()
-)
-->addTest(
-	/* #4 : Criar fatura de fornecedor e associar a um ficheiro */
-	(new UnitTest())
-	->describe("Criar fatura de fornecedor e associar a um ficheiro")
+	->describe("Criar fatura de fornecedor e associar a um ficheiro inexistente e receber erro")
 	->expectQuery('CALL FF_NovaFatura("ficheironaoexistente.pdf");')
 	->toErrWithCode("23000")
+)
+->addTest(
+	(new UnitTest())
+	->describe("Criar fatura de fornecedor e associar a um ficheiro existente")
+	->expectQuery('INSERT INTO SYS_Files (Id) VALUES ("12345.pdf"); CALL FF_NovaFatura("12345.pdf");')
+	->toSucceed()
+)
+->addTest(
+	(new UnitTest())
+	->describe("Classificar fatura de fornecedor com múltiplos campos")
+	->expectQuery('CALL FF_ClassificarFornecedor("12345.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
+	->toSucceed()
 )
 	
 ->go();
