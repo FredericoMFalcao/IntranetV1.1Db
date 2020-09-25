08ac30cce719f6296258f12ea33325f3ec7df961
Frederico <FredericoMFalcao@hotmail.com>
2020-09-25 17:18:48 +0100

    Update 00_build_support_funcs.sql

--- 01_sql/00_build_support_funcs.sql
+++ 01_sql/00_build_support_funcs.sql
@@ -1,14 +1,16 @@
 <?php
 // vim: syntax=php
 
-function tableNameWithModule(string $tblName) {
-  $key = array_search(__FUNCTION__, array_column(debug_backtrace(), 'function'));
-  $callerFileNameAndPath = debug_backtrace()[$key]['file'];
+function tableNameWithModule(string $tblName, string $moduleName) {
+//   $key = array_search(__FUNCTION__, array_column(debug_backtrace(), 'function'));
+//   $callerFileNameAndPath = debug_backtrace()[$key]['file'];
 
-  $path = explode("/",$callerFileNameAndPath);
-  $moduleName = $path[count($path)-3];
+//  $path = explode("/",$callerFileNameAndPath);
+//  $moduleName = $path[count($path)-3];
   
-  return strtoupper(substr($moduleName, 3))."_".$tblName;
+//  return strtoupper(substr($moduleName, 3))."_".$tblName;
+  
+  return strtoupper($moduleName)."_".$tblName;
 
 }
 ?>
