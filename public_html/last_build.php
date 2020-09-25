5112601564b8e83c4bdaa7b2b5a8bbfc3fa02648
Frederico <FredericoMFalcao@hotmail.com>
2020-09-25 15:14:07 +0100

    Changed order of content

--- public_html/last_build.php
+++ public_html/last_build.php
@@ -1,22 +1,26 @@
 <html>
-<head>
-</head>
+	<head>
+	</head>
 <body>
-<h2>Links</h2>
-<ul>
-	<li>	<a href="sql_explorer.php">sql explorer</a></li>
-	<li>	<a href="deploy_code.sh">force code deployment</a></li>
-</ul>
-<hr>
-<h2>Last WebServer Errors</h2>
-<pre><?php passthru("tail ../log/apache2_error.log");  ?></pre>
-<hr>
-<h2>Last Compilation Status</h2>
-<pre><?php echo file_get_contents("./last_update_error.txt"); ?></pre>
-<hr>
-<h2>Last Compilation Text</h2>
-<pre><?php echo file_get_contents("./last_compiled_code.txt"); ?></pre>
-<h2>Last Unit Test Results</h2>
-<pre><?php echo file_get_contents("./last_unit_test_results.html"); ?></pre>
+	<h2>Links</h2>
+		<ul>
+			<li>	<a href="sql_explorer.php">sql explorer</a></li>
+			<li>	<a href="deploy_code.sh">force code deployment</a></li>
+		</ul>
+	<hr>
+	
+	<h2>Last WebServer Errors</h2>
+	<pre><?php passthru("tail ../log/apache2_error.log");  ?></pre>
+	<hr>
+	
+	<h2>Last Compilation Status</h2>
+	<pre><?php echo file_get_contents("./last_update_error.txt"); ?></pre>
+	<hr>
+	
+	<h2>Last Unit Test Results</h2>
+	<pre><?php echo file_get_contents("./last_unit_test_results.html"); ?></pre>
+
+	<h2>Last Compilation Text</h2>
+	<pre><?php echo file_get_contents("./last_compiled_code.txt"); ?></pre>
 </body>
 </html>
