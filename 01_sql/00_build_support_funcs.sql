8615def695c214688ed019b1e57a00f735256ffa
Frederico <FredericoMFalcao@hotmail.com>
2020-09-25 16:47:53 +0100

    Create 00_build_support_funcs.sql

---
+++ 01_sql/00_build_support_funcs.sql
@@ -0,0 +1,13 @@
+<?php
+
+
+function prependModule(string $tblName) {
+  $key = array_search(__FUNCTION__, array_column(debug_backtrace(), 'function'));
+  $callerFileNameAndPath = debug_backtrace()[$key]['file'];
+
+  $path = explode("/",$callerFileNameAndPath);
+  $moduleName = $path[count($path)-3];
+  
+  return strtoupper(substr($moduleName, 3))."_".$tblName;
+
+}
