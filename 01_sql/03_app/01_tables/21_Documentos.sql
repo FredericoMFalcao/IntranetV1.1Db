74cb3281da7dee85e18481146c835e5d0b21d4cb
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 10:52:29 +0100

    passa a usar a função 'tableNameWithModule'

--- 01_sql/03_app/01_tables/21_Documentos.sql
+++ 01_sql/03_app/01_tables/21_Documentos.sql
@@ -1,16 +1,16 @@
 -- Tabela: Documentos
 -- Descricao: Lista todos os códigos e propriedades dos documentos inseridos na aplicação
 
-CREATE TABLE Documentos (
-  NumSerie VARCHAR(25),
+CREATE TABLE <?=tableNameWithModule()?> (
+  NumSerie VARCHAR(25), -- unique key
   
   Tipo VARCHAR(50) NOT NULL, -- foreign key
   Estado VARCHAR(50) NOT NULL, -- foreign key
   
-  FileId VARCHAR(255),
+  FileId VARCHAR(255) NOT NULL, -- foreign key
   Extra JSON,
   
   FOREIGN KEY (FileId) REFERENCES SYS_Files(Id),
   UNIQUE (NumSerie),
-  FOREIGN KEY (Tipo,Estado) REFERENCES DocEstados(TipoDoc,Estado) ON DELETE RESTRICT ON UPDATE CASCADE
+  FOREIGN KEY (Tipo,Estado) REFERENCES <?=tableNameWithModule("DocEstados")?>(TipoDoc,Estado) ON DELETE RESTRICT ON UPDATE CASCADE
 );
