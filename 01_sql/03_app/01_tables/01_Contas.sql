603bfe44af3b77ed945c9c262308b69b2d0ef846
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 10:37:42 +0100

    passa a usar a função 'tableNameWithModule'

--- 01_sql/03_app/01_tables/01_Contas.sql
+++ 01_sql/03_app/01_tables/01_Contas.sql
@@ -2,7 +2,7 @@
 -- Descrição: Lista todos os nomes e propriedades das contas contabilísticas da aplicação
 
 CREATE TABLE <?=tableNameWithModule()?> (
-  Conta VARCHAR(20) NOT NULL, -- Dois primeiros digitos reflectem o tipo de conta
+  Conta VARCHAR(20) NOT NULL, -- Dois primeiros caracteres reflectem o tipo de conta
   Nome VARCHAR(255) NOT NULL,
   Extra JSON,
   PRIMARY KEY (Conta)
@@ -10,10 +10,10 @@ CREATE TABLE <?=tableNameWithModule()?> (
 
 
 -- - Dados Iniciais -- -
-INSERT INTO Contas (Conta, Nome) VALUES ('CR0101',"Teste01"); -- centro de resultados
-INSERT INTO Contas (Conta, Nome) VALUES ('AN0202',"Teste02"); -- analítica
-INSERT INTO Contas (Conta, Nome) VALUES ('FO32121000',"Teste03"); -- fornecedor
-INSERT INTO Contas (Conta, Nome) VALUES ('COabc',"Teste04"); -- colaborador
-INSERT INTO Contas (Conta, Nome) VALUES ('CG01',"FaturasFornecedor"); -- custos gerais
+INSERT INTO <?=tableNameWithModule()?> (Conta, Nome) VALUES ('CR0101',"Teste01"); -- centro de resultados
+INSERT INTO <?=tableNameWithModule()?> (Conta, Nome) VALUES ('AN0202',"Teste02"); -- analítica
+INSERT INTO <?=tableNameWithModule()?> (Conta, Nome) VALUES ('FO32121000',"Teste03"); -- fornecedor
+INSERT INTO <?=tableNameWithModule()?> (Conta, Nome) VALUES ('COabc',"Teste04"); -- colaborador
+INSERT INTO <?=tableNameWithModule()?> (Conta, Nome) VALUES ('CG01',"FaturasFornecedor"); -- custos gerais
 
 -- vim: syntax=sql
