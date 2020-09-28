696b6000578d6ab0a2daa42d6d54b3d050c3fca3
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 17:07:55 +0100

    Create VIW_FF_PorClassificarFornecedor.sql

---
+++ 01_sql/03_app/03_views/VIW_FF_PorClassificarFornecedor.sql
@@ -0,0 +1,12 @@
+-- View: VIW_FF_PorClassificarFornecedor
+-- Descrição: Lista as faturas de fornecedores no estado 'PorClassificarFornecedor'
+-- Depende de: tabela 'Documentos'
+
+DROP VIEW IF EXISTS VIW_FF_PorClassificarFornecedor;
+
+CREATE VIEW VIW_FF_PorClassificarFornecedor AS
+SELECT FileId
+FROM <?=tableNameWithModule("Documentos")?> 
+WHERE Tipo = 'FaturaFornecedor'
+  AND a.Estado = 'PorClassificarFornecedor'
+;
