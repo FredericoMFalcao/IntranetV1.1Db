e952bb5ea05c14f266660e47f9e7f4352762dede
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 13:08:52 +0100

    fixed indentation

--- 01_sql/03_app/03_views/VIW_Fornecedores.sql
+++ 01_sql/03_app/03_views/VIW_Fornecedores.sql
@@ -6,10 +6,10 @@ DROP VIEW IF EXISTS VIW_Fornecedores;
 
 CREATE VIEW VIW_Fornecedores AS
 SELECT
-	Conta AS Codigo,
-	Nome,
-	JSON_EXTRACT (Extra, '$.NomeCurto') AS NomeCurto,
-	JSON_EXTRACT (Extra, '$.NIF') AS NIF
+  Conta AS Codigo,
+  Nome,
+  JSON_EXTRACT (Extra, '$.NomeCurto') AS NomeCurto,
+  JSON_EXTRACT (Extra, '$.NIF') AS NIF
 FROM <?=tableNameWithModule("Contas")?>
 WHERE LEFT(Conta,2) = 'FO'
 ;
