171c4407735c8d625e1e2f8f627198292af66e88
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 12:26:21 +0100

    adicionei comentários

--- 01_sql/03_app/02_functions/ExtrairConta.sql
+++ 01_sql/03_app/02_functions/ExtrairConta.sql
@@ -1,6 +1,9 @@
+-- Descrição: para uma dada conta multipla (e.g. CR0101:AN0202), devolve a conta do tipo dado como argumento
+
 DROP FUNCTION IF EXISTS ExtrairConta;
 
 CREATE FUNCTION ExtrairConta (Tipo CHAR(2), Conta_Multi TEXT)
 RETURNS TEXT
 RETURN
   SUBSTRING_INDEX(SUBSTRING(Conta_Multi, LOCATE(Tipo, Conta_Multi)), ":", 1);
+  -- 'SUBSTRING' devolve tudo a partir do tipo de conta dado no 'LOCATE'; 'SUBSTRING_INDEX' pega nesse string e devolve tudo o que está à esquerda do primeiro ':'
