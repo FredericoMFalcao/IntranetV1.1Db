b9a7d27158ed0b8d5e0ca787a0001a99a81a7356
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 10:46:38 +0100

    passa a usar a função 'tableNameWithModule'

--- 01_sql/03_app/01_tables/03_DocEstados.sql
+++ 01_sql/03_app/01_tables/03_DocEstados.sql
@@ -1,7 +1,7 @@
 -- Tabela: DocEstados
 -- Descricao: Lista todos os estados que cada tipo de documento pode ter
 
-CREATE TABLE DocEstados (  
+CREATE TABLE <?=tableNameWithModule()?> (  
   TipoDoc VARCHAR(50) NOT NULL,
   Estado VARCHAR(50) NOT NULL,
   Descricao VARCHAR(255),
@@ -12,9 +12,9 @@ CREATE TABLE DocEstados (
 
 
 -- Dados Iniciais
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarFornecedor', 'Existe no sistema apenas com um PDF.');
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarAnalitica',  'À espera de ser classificada em termos de contabilidade analítica.');
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorAnexarCPagamento',     'À espera de ser anexado comprovativo de pagamento.');
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarContabilidade', 'À espera de integrar custo no software de contabilidade fiscal.');
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarPagamentoContab', 'À espera de integrar pagamento no software de contabilidade fiscal.');
-INSERT INTO DocEstados (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'Concluido', 'Todos os procedimentos foram realizados.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarFornecedor', 'Existe no sistema apenas com um PDF.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorClassificarAnalitica',  'À espera de ser classificada em termos de contabilidade analítica.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorAnexarCPagamento',     'À espera de ser anexado comprovativo de pagamento.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarContabilidade', 'À espera de integrar custo no software de contabilidade fiscal.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'PorRegistarPagamentoContab', 'À espera de integrar pagamento no software de contabilidade fiscal.');
+INSERT INTO <?=tableNameWithModule()?> (TipoDoc, Estado, Descricao) VALUES ('FaturaFornecedor', 'Concluido', 'Todos os procedimentos foram realizados.');
