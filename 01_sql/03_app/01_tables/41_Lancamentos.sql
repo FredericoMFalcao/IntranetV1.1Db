fbfafd9264b765bad96b64918f9a0225720024a7
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-28 10:59:43 +0100

    passa a usar a função 'tableNameWithModule'

--- 01_sql/03_app/01_tables/41_Lancamentos.sql
+++ 01_sql/03_app/01_tables/41_Lancamentos.sql
@@ -1,8 +1,8 @@
 -- Tabela: Lancamentos
 -- Descricao: Regista todos os lançamentos contabilísticos da empresa
 
-CREATE TABLE Lancamentos (
-  Conta        VARCHAR(30) NOT NULL, -- pode ser conta composta (e.g. '0101:020403:ABC' para Centro Resultados, Analítica e Colaborador)
+CREATE TABLE <?=tableNameWithModule()?> (
+  Conta        VARCHAR(30) NOT NULL, -- pode ser conta composta (e.g. 'CR0101:AN020403:COabc' para Centro Resultados, Analítica e Colaborador)
   CoefRateio   FLOAT NOT NULL,       -- percentagem do valor total do respectivo documento (pode variar entre -1 e 1)
   Mes          DATE NOT NULL,        -- primeiro dia do mês a que o valor diz respeito
   DocNumSerie  VARCHAR(25) NOT NULL, -- foreign key
