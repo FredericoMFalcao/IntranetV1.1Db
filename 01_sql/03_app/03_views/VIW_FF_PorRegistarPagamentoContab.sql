e953db3f5ad6dfb931c3701cd6d456c6b99bcc99
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-29 11:15:41 +0100

    fixed typo

--- 01_sql/03_app/03_views/VIW_FF_PorRegistarPagamentoContab.sql
+++ 01_sql/03_app/03_views/VIW_FF_PorRegistarPagamentoContab.sql
@@ -7,7 +7,7 @@ DROP VIEW IF EXISTS VIW_FF_PorRegistarPagamentoContab;
 CREATE VIEW VIW_FF_PorRegistarPagamentoContab AS
 SELECT
 	a.FileId,
-	'CPagamento123.pdf' AS DocComprovativo,                                  -- provisório
+	'CPagamento123.pdf' AS ComprovativoPagamento,                            -- provisório
 	a.NumSerie,
 	CONVERT(JSON_EXTRACT (a.Extra, '$.DataFatura'), DATE) AS DataFatura,
 	c.Nome AS FornecedorNome,
