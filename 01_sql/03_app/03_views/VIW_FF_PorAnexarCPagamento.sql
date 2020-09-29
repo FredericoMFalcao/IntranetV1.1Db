2152e773f4ace46f366b512641317a91fda94419
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-29 09:50:59 +0100

    Create VIW_FF_PorAnexarCPagamento.sql

---
+++ 01_sql/03_app/03_views/VIW_FF_PorAnexarCPagamento.sql
@@ -0,0 +1,27 @@
+-- View: VIW_FF_PorAnexarCPagamento
+-- Descrição: Lista todas as faturas de fornecedores no estado 'PorAnexarCPagamento'
+-- Depende de: tabelas 'Documentos', 'Lancamentos' e 'Contas'
+
+DROP VIEW IF EXISTS VIW_FF_PorAnexarCPagamento;
+
+CREATE VIEW VIW_FF_PorAnexarCPagamento AS
+SELECT
+	a.FileId,
+	a.NumSerie,
+	CONVERT(JSON_EXTRACT (a.Extra, '$.DataFatura'), DATE) AS DataFatura,
+	JSON_EXTRACT (a.Extra, '$.Projeto') AS Projeto,
+	c.Nome AS FornecedorNome,
+	JSON_EXTRACT (c.Extra, '$.FornecedorNIF') AS FornecedorNIF,
+	JSON_EXTRACT (a.Extra, '$.NumFatura') AS NumFatura,
+	FF_ValorTotal(a.Extra) AS ValorTotal,
+	0 AS Retencao,                                          -- provisório
+	FF_ValorTotal(a.Extra) - 0 AS ValorLiquido,             -- provisório
+	JSON_EXTRACT (a.Extra, '$.Moeda') AS Moeda,
+	'CPagamento123.pdf' AS DocComprovativo                  -- provisório
+FROM <?=tableNameWithModule("Documentos")?> AS a
+INNER JOIN <?=tableNameWithModule("Lancamentos")?> AS b ON a.NumSerie = b.DocNumSerie
+INNER JOIN <?=tableNameWithModule("Contas")?> AS c ON b.Conta = c.Conta
+WHERE a.Tipo = 'FaturaFornecedor'
+	AND LEFT(c.Conta,2) = 'FO'
+	AND a.Estado = 'PorAnexarCPagamento'
+;
