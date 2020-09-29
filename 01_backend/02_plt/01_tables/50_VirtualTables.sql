1a35a44e4c39ca1777cba76086de1b84dafa8bf4
Frederico <FredericoMFalcao@hotmail.com>
2020-09-29 12:48:56 +0100

    Added functionality to handle virtual tables in SQL and WebBrowser

--- 01_backend/02_plt/01_tables/50_VirtualTables.sql
+++ 01_backend/02_plt/01_tables/50_VirtualTables.sql
@@ -5,6 +5,24 @@
 --                    - metadata helps the browser to know how to auto-build create/edit forms and parse text data into images, icons, etc... 
 CREATE TABLE PLT_VirtualTables (
   Name VARCHAR(255) NOT NULL,
+  
+  -- SQL CRUD Operations:
+  -- Fields that will hold SQL code to perform C.R.U.D. operations on these virtual tables
+  -- N.B.: if any of these are NULL, code will be automatically generated during the "build" process based on the table Fields
+  SQLCreateStoredProcedure TEXT,
+  SQLReadStoredProcedure TEXT,
+  SQLUpdateStoredProcedure TEXT,
+  SQLDeleteStoredProcedure TEXT,
+
+  -- JS Table, CreateNewForm and EditForm:
+  -- Fields that will hold a JS function that produces a DomElement
+  -- N.B.: if any of these are NULL, code will be automatically generated during the "build" process based on the table Fields
+  JSCreateStoredProcedure TEXT,
+  JSReadStoredProcedure TEXT,
+  JSUpdateStoredProcedure TEXT,
+  JSDeleteStoredProcedure TEXT,
+
+  
   PrimaryKey JSON,
   PRIMARY KEY (Name)
    
@@ -14,20 +32,29 @@ CREATE TABLE PLT_VirtualTablesFields (
   ParentTable VARCHAR(255), -- foreign key
   Name VARCHAR(255) NOT NULL,
   Type VARCHAR(25) NOT NULL,
+  AllowNull INT DEFAULT 0,
+  DefaultValue TEXT DEFAULT "",
   Options JSON,
   FOREIGN KEY (ParentTable) REFERENCES PLT_VirtualTables(Name) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 -- -- INITIAL DATA ----
-INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('PorClassificarFornecedor','["NumSerie"]');
-INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('PorClassificarFornecedor','NumSerie','TEXT');
+INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('Fornecedor','["Codigo"]');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Fornecedor','Codigo','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Fornecedor','Nome','TEXT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Fornecedor','NomeCurto','TEXT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Fornecedor','NIF','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Fornecedor','Morarada','TEXT');
 
-INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('PorClassificarAnalitica','["NumSerie"]');
-INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('PorClassificarAnalitica',"NumSerie",'TEXT');
+INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('CentroResultado','["Codigo"]');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('CentroResultado','Codigo','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('CentroResultado','Nome','TEXT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('CentroResultado','PermitirCustos','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('CentroResultado','PermitirProveitos','INT');
 
-INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('Contas','["Conta","Tipo"]');
-INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES 
-  ('Contas',"Conta",'TEXT'),
-  ('Contas',"Nome",'TEXT'),
-  ('Contas',"Tipo",'TEXT');
+INSERT INTO PLT_VirtualTables (Name, PrimaryKey) VALUES ('Analitica','["Codigo"]');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Analitica','Codigo','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Analitica','Nome','TEXT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Analitica','PermitirCustos','INT');
+INSERT INTO PLT_VirtualTablesFields (ParentTable, Name, Type) VALUES ('Analitica','PermitirProveitos','INT');
