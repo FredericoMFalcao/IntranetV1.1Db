d38f0d156a4316bb10a040dbaa6b97f02493bf16
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 19:08:02 +0100

    testes alterados para novo modelo (só até classificação analitica)

--- 01_backend/03_app/05_tests/01_PorClassificarFornecedor.php
+++ 01_backend/03_app/05_tests/01_PorClassificarFornecedor.php
@@ -6,20 +6,27 @@ require_once __DIR__."/_tests_lib.php";
 (new TestSuite("Faturas Fornecedor"))
 ->addTest(
 	(new UnitTest())
-	->describe("Criar fatura de fornecedor e associar a um ficheiro inexistente e receber erro")
-	->expectQuery('CALL FF_NovaFatura("ficheironaoexistente.pdf");')
+	->describe("Criar fatura de fornecedor, associar a um ficheiro inexistente e receber erro")
+	->expectQuery('CALL DocumentosCriarNovaFaturaFornecedor ("ficheironaoexistente.pdf");')
 	->toErrWithCode("23000")
 )
 ->addTest(
 	(new UnitTest())
 	->describe("Criar fatura de fornecedor e associar a um ficheiro existente")
-	->expectQuery('INSERT INTO SYS_Files (Id) VALUES ("12345.pdf"); CALL FF_NovaFatura("12345.pdf");')
+	->expectQuery('INSERT INTO SYS_Files (Id) VALUES ("fatura123.pdf"); CALL DocumentosCriarNovaFaturaFornecedor ("fatura123.pdf");')
+	// Fatura é criada na tabela 'Documentos' com id = 1
 	->toSucceed()
 )
 ->addTest(
 	(new UnitTest())
-	->describe("Classificar fatura de fornecedor com múltiplos campos")
-	->expectQuery('CALL FF_ClassificarFornecedor("12345.pdf","NumFatura","01 Geral","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-25\", \"Fim\": \"2011-11-25\"}","2012-12-12","0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste");')
+	->describe("Classificar fornecedor")
+	->expectQuery('CALL DocumentoAprovar (1,"FTAn12#123.pdf","123","01","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-01\", \"Fim\": \"2011-12-31\"}","2012-12-12","FO0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste",0,0,NULL,NULL);')
+	->toSucceed()
+)
+->addTest(
+	(new UnitTest())
+	->describe("Classificar analítica")
+	->expectQuery('CALL DocumentoAprovar (1,"FTAn12#123.pdf",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"[{\"CentroResultados\": \"CR0101\", \"Analitica\": \"AN0202\", \"Colaborador\": \"CO123\", \"Valor\": 1000}]",NULL);')
 	->toSucceed()
 )
 	
