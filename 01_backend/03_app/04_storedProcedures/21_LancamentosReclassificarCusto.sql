872f553558f01c294569d4e6b56c67682f0f31cf
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 19:00:18 +0100

    added prefix 'v' to local variables

--- 01_backend/03_app/04_storedProcedures/21_LancamentosReclassificarCusto.sql
+++ 01_backend/03_app/04_storedProcedures/21_LancamentosReclassificarCusto.sql
@@ -8,18 +8,18 @@ CREATE PROCEDURE LancamentosReclassificarCusto (
   -- e.g. [{"CentroResultados": "CR0101", "Analitica": "AN0202", "Colaborador": "COabc", "Valor": 1000}, {...}]
 )
   BEGIN
-    DECLARE ValorFatura DECIMAL(18,2);
-    DECLARE PeriodoFaturacao TEXT;
+    DECLARE v_ValorFatura DECIMAL(18,2);
+    DECLARE v_PeriodoFaturacao TEXT;
     DECLARE i INT;
-    SET ValorFatura = FF_ValorTotal((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie));
-    SET PeriodoFaturacao = JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.PeriodoFaturacao');
+    SET v_ValorFatura = FF_ValorTotal((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie));
+    SET v_PeriodoFaturacao = JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.PeriodoFaturacao');
     SET i = 0;
     
     -- 1. Anular lançamentos em custos gerais / apagar lançamentos em custos específicos
     IF EXISTS (SELECT DocNumSerie FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = in_NumSerie AND LEFT(Conta, 2) = 'CR') THEN
       DELETE FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = in_NumSerie AND LEFT(Conta, 2) = 'CR';
     ELSE
-      CALL CriarLancamento ("CG01", 1, PeriodoFaturacao, in_NumSerie);
+      CALL CriarLancamento ("CG01", 1, v_PeriodoFaturacao, in_NumSerie);
     END IF;
 
     -- 2. Inserir lançamentos em custos específicos
@@ -31,8 +31,8 @@ CREATE PROCEDURE LancamentosReclassificarCusto (
           JSON_EXTRACT(JSON_EXTRACT(in_ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Analitica'),
           JSON_EXTRACT(JSON_EXTRACT(in_ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Colaborador')
         ),
-        JSON_EXTRACT(JSON_EXTRACT(in_ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Valor') / ValorFatura * -1,
-        PeriodoFaturacao,
+        JSON_EXTRACT(JSON_EXTRACT(in_ClassificacaoAnalitica, CONCAT("'$[", i, "]'")), '$.Valor') / v_ValorFatura * -1,
+        v_PeriodoFaturacao,
         in_NumSerie
       );
 
