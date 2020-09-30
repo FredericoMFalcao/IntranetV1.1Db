316d41aa9426a0977b1f7ac77777d3396b1b3266
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 13:05:16 +0100

    criei procedimento

--- 01_backend/03_app/04_storedProcedures/13_DocumentosCriarNovaFaturaFornecedor.sql
+++ 01_backend/03_app/04_storedProcedures/13_DocumentosCriarNovaFaturaFornecedor.sql
@@ -1,1 +1,28 @@
--- placeholder
+DROP PROCEDURE IF EXISTS DocumentosCriarNovaFaturaFornecedor;
+
+DELIMITER //
+
+-- Descrição: transforma um ficheiro no disco numa fatura de fornecedor por classificar
+--
+--        será chamada pelo sistema quando:
+--         (1) recebe um email com anexo PDF
+--         (2) ficheiro Dropbox
+--
+
+CREATE PROCEDURE DocumentosCriarNovaFaturaFornecedor (IN _FileId TEXT)
+  BEGIN
+ 
+    -- 0. Verificar validade dos argumentos
+    IF NOT EXISTS (SELECT Id FROM SYS_Files WHERE Id = _FileId)
+      THEN signal sqlstate '23000' set message_text = 'FileId inexistente.';
+    END IF;
+   
+    -- 1. Inserir em Documentos 
+    INSERT INTO <?=tableNameWithModule("Documentos")?> (Tipo, Estado, FileId) 
+    VALUES ('FaturaFornecedor', 'PorClassificarFornecedor', _FileId);
+  
+  END;
+  
+//
+
+DELIMITER ;
