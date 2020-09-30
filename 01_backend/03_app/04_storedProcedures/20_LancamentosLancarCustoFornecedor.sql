bb46253195ceeb8af02b191d1ad8ac40c00f9bbc
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 09:22:39 +0100

    criei procedimento

--- 01_backend/03_app/04_storedProcedures/20_LancamentosLancarCustoFornecedor.sql
+++ 01_backend/03_app/04_storedProcedures/20_LancamentosLancarCustoFornecedor.sql
@@ -1,1 +1,32 @@
--- placeholder
+DROP PROCEDURE IF EXISTS LancamentosLancarCustoFornecedor;
+
+DELIMITER //
+
+CREATE PROCEDURE LancamentosLancarCustoFornecedor (
+	IN _NumSerie                 TEXT,
+	IN _PeriodoFaturacao         TEXT,
+	-- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
+	IN _FornecedorCodigo         TEXT
+)
+	BEGIN
+ 
+ 		-- 0. Verificar validade dos argumentos
+		IF _NumSerie NOT IN (SELECT NumSerie FROM <?=tableNameWithModule("Documentos")?>)
+			THEN signal sqlstate '20000' set message_text = 'Fatura inexistente ou indisponível para esta ação';
+		END IF;
+		
+		-- 1. Lançar em custos gerais / anular lançamentos em fornecedor
+		IF _NumSerie IN (SELECT NumSerie FROM <?=tableNameWithModule("Lancamentos")?> WHERE LEFT(Conta, 2) = FO)
+			DELETE FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = _NumSerie AND LEFT(Conta, 2) = FO;
+		ELSE
+			CALL CriarLancamento ("CG01", -1, _PeriodoFaturacao, _NumSerie);
+		END IF;
+					   
+		-- 2. Inserir lançamentos na conta do fornecedor
+		CALL CriarLancamento (_FornecedorCodigo, 1, _PeriodoFaturacao, _NumSerie);
+  
+	END;
+	
+//
+
+DELIMITER ;
