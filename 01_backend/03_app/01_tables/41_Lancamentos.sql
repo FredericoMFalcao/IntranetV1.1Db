e7b4efb480baf84593ea956a16ba481109165312
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 15:00:30 +0100

    anulei alteração anterior

--- 01_backend/03_app/01_tables/41_Lancamentos.sql
+++ 01_backend/03_app/01_tables/41_Lancamentos.sql
@@ -5,8 +5,8 @@ CREATE TABLE <?=tableNameWithModule()?> (
   Conta        VARCHAR(30) NOT NULL, -- pode ser conta composta (e.g. 'CR0101:AN020403:COabc' para Centro Resultados, Analítica e Colaborador)
   CoefRateio   FLOAT NOT NULL,       -- percentagem do valor total do respectivo documento (pode variar entre -1 e 1)
   Mes          DATE NOT NULL,        -- primeiro dia do mês a que o valor diz respeito
-  DocumentoId  INT NOT NULL,         -- foreign key
+  DocNumSerie  VARCHAR(20) NOT NULL, -- foreign key
   
-  PRIMARY KEY (Conta,CoefRateio,Mes,DocumentoId),
-  FOREIGN KEY (DocumentoId) REFERENCES <?=tableNameWithModule("Documentos")?> (Id) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (Conta,CoefRateio,Mes,DocNumSerie),
+  FOREIGN KEY (DocNumSerie) REFERENCES <?=tableNameWithModule("Documentos")?> (NumSerie) ON DELETE CASCADE ON UPDATE CASCADE
 );
