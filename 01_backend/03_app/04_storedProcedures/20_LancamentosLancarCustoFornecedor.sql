185880eeb827d4c1921d2f07e055e3114729b46a
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-09-30 17:51:33 +0100

    added prefix 'in' to input variables

--- 01_backend/03_app/04_storedProcedures/20_LancamentosLancarCustoFornecedor.sql
+++ 01_backend/03_app/04_storedProcedures/20_LancamentosLancarCustoFornecedor.sql
@@ -3,22 +3,22 @@ DROP PROCEDURE IF EXISTS LancamentosLancarCustoFornecedor;
 DELIMITER //
 
 CREATE PROCEDURE LancamentosLancarCustoFornecedor (
-  IN _NumSerie                 TEXT,
-  IN _PeriodoFaturacao         TEXT,
+  IN in_NumSerie                 TEXT,
+  IN in_PeriodoFaturacao         TEXT,
   -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
-  IN _FornecedorCodigo         TEXT
+  IN in_FornecedorCodigo         TEXT
 )
   BEGIN
 
     -- 1. Lançar em custos gerais / apagar lançamentos em fornecedor
-    IF EXISTS (SELECT DocNumSerie FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = _NumSerie AND LEFT(Conta, 2) = 'FO') THEN
-      DELETE FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = _NumSerie AND LEFT(Conta, 2) = 'FO';
+    IF EXISTS (SELECT DocNumSerie FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = in_NumSerie AND LEFT(Conta, 2) = 'FO') THEN
+      DELETE FROM <?=tableNameWithModule("Lancamentos")?> WHERE DocNumSerie = in_NumSerie AND LEFT(Conta, 2) = 'FO';
     ELSE
-      CALL CriarLancamento ("CG01", -1, _PeriodoFaturacao, _NumSerie);
+      CALL CriarLancamento ("CG01", -1, in_PeriodoFaturacao, in_NumSerie);
     END IF;
 					   
     -- 2. Inserir lançamentos na conta do fornecedor
-    CALL CriarLancamento (_FornecedorCodigo, 1, _PeriodoFaturacao, _NumSerie);
+    CALL CriarLancamento (in_FornecedorCodigo, 1, in_PeriodoFaturacao, in_NumSerie);
   
   END;
 	
