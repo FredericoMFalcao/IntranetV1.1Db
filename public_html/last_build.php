b9958df6c340a12c74ca568e25a964c9cc449ea7
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-10-01 16:03:58 +0000

    added syntax highlighting capability

--- public_html/last_build.php
+++ public_html/last_build.php
@@ -1,10 +1,20 @@
 <html>
 	<head>
+		<!-- BOOTSTRAP LIBRARIES -->
+
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 		
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+		<!-- highlight.js LIBRARIES -->
+		<link rel="stylesheet" href="/css/default.css">
+		<script src="/js/highlight.min.js"></script>
+		<script>hljs.initHighlightingOnLoad();</script>
+
+			
+
 	</head>
 <body class="container">
 	<h2>Links</h2>
@@ -45,7 +55,7 @@
 
 	    <div id="collapseGroup<?=$i?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
 	      <div class="card-body">
-		      <pre><?= htmlspecialchars($content['body'])?></pre>
+		      <pre><code class="language-sql" ><?= htmlspecialchars($content['body'])?></code></pre>
 	      </div>
 	    </div>
 	  </div>
