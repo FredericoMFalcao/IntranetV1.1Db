c71371a3e5ac13c252e07f1ff70eda3cdd0d3e72
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-01 16:57:29 +0100

    corrigido para somar as componentes Base e IVA de cada tipo

--- 01_backend/03_app/02_functions/FF_ValorTotal.sql
+++ 01_backend/03_app/02_functions/FF_ValorTotal.sql
@@ -1,29 +1,11 @@
--- Devolve o valor total da fatura (soma de todos os componentes do campo 'Valor' do campo 'Extra', e.g. ValorBaseBens + IVABens + ValorBaseServicos + IVAServicos)
+-- Devolve o valor total da fatura, somando todos os componentes do campo 'Valor' do campo 'Extra' do documento
+-- Formato do campo 'Valor': {"Bens": {"ValorBase": 0.00, "Iva": 0.00}, "Servicos": {"ValorBase": 0.00, "Iva": 0.00}}
 
 DROP FUNCTION IF EXISTS FF_ValorTotal;
 
-DELIMITER //
-
 CREATE FUNCTION FF_ValorTotal (Extra TEXT)
 RETURNS DECIMAL(18,2)
-BEGIN
-  DECLARE i INT;
-  DECLARE Valor DECIMAL(18,2);
-  SET i = 0;
-  SET Valor = 0;
-
-  WHILE i != JSON_LENGTH(JSON_EXTRACT (Extra, '$.Valor')) DO
-  
-    SET Valor = Valor + JSON_EXTRACT(JSON_EXTRACT(Extra, '$.Valor'), CONCAT("$.", JSON_EXTRACT(JSON_KEYS(JSON_EXTRACT(Extra, '$.Valor')), CONCAT("$[", i, "]"))));
-	
-	SET i = i + 1;
-
-  END WHILE;
-  
-  RETURN Valor;
-
-END;
-
-//
-  
-DELIMITER ;
+RETURN JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(Extra, '$.Valor'), '$.Bens'), '$.ValorBase') +
+       JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(Extra, '$.Valor'), '$.Bens'), '$.Iva') +
+       JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(Extra, '$.Valor'), '$.Servicos'), '$.ValorBase') +
+       JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(Extra, '$.Valor'), '$.Servicos'), '$.Iva');
