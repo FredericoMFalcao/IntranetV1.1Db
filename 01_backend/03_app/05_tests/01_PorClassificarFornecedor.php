1c6e2687d947dad8df7058461006dedf0332bb21
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-01 14:26:35 +0100

    Update 01_PorClassificarFornecedor.php

--- 01_backend/03_app/05_tests/01_PorClassificarFornecedor.php
+++ 01_backend/03_app/05_tests/01_PorClassificarFornecedor.php
@@ -7,26 +7,42 @@ require_once __DIR__."/_tests_lib.php";
 ->addTest(
 	(new UnitTest())
 	->describe("Criar fatura de fornecedor, associar a um ficheiro inexistente e receber erro")
-	->expectQuery('CALL DocumentosCriarNovaFaturaFornecedor ("ficheironaoexistente.pdf");')
+	->expectQuery('
+		CALL DocumentosCriarNovaFaturaFornecedor ("ficheironaoexistente.pdf");
+	')
 	->toErrWithCode("23000")
 )
 ->addTest(
 	(new UnitTest())
 	->describe("Criar fatura de fornecedor e associar a um ficheiro existente")
-	->expectQuery('INSERT INTO SYS_Files (Id) VALUES ("fatura123.pdf"); CALL DocumentosCriarNovaFaturaFornecedor ("fatura123.pdf");')
-	// Fatura é criada na tabela 'Documentos' com id = 1
+	->expectQuery('
+		INSERT INTO SYS_Files (Id) VALUES ("fatura123.pdf");
+		CALL DocumentosCriarNovaFaturaFornecedor ("fatura123.pdf");
+	')
 	->toSucceed()
 )
 ->addTest(
 	(new UnitTest())
 	->describe("Classificar fornecedor")
-	->expectQuery('CALL DocumentoAprovar (1,"FTAn12#123.pdf","123","01","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-01\", \"Fim\": \"2011-12-31\"}","2012-12-12","FO0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste",0,0,NULL,NULL);')
+	->expectQuery('
+		CALL DocumentoAprovar (2,"FTAn12#123.pdf","123","01","2012-12-12","2012-12-12","{\"Inicio\": \"2011-11-01\", \"Fim\": \"2011-12-31\"}","2012-12-12","FO0000111","{\"Bens\": {\"ValorBase\": 0.00, \"Iva\": 0.00}, \"Servicos\": {\"ValorBase\":0.00,\"Iva\":0.00}}","AKZ","Fatura de teste",0,0,NULL,NULL);
+	')
 	->toSucceed()
 )
 ->addTest(
 	(new UnitTest())
 	->describe("Classificar analítica")
-	->expectQuery('CALL DocumentoAprovar (1,"FTAn12#123.pdf",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"[{\"CentroResultados\": \"CR0101\", \"Analitica\": \"AN0202\", \"Colaborador\": \"CO123\", \"Valor\": 1000}]",NULL);')
+	->expectQuery('
+		CALL DocumentoAprovar (2,"FTAn12#123.pdf",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"[{\"CentroResultados\": \"CR0101\", \"Analitica\": \"AN0202\", \"Valor\": 1000}]",NULL);
+	')
+	->toSucceed()
+)
+->addTest(
+	(new UnitTest())
+	->describe("Classificar analítica")
+	->expectQuery('
+		CALL DocumentoAprovar (2,"FTAn12#123.pdf",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"[{\"CentroResultados\": \"CR0101\", \"Analitica\": \"AN0202\", \"Colaborador\": \"CO123\", \"Valor\": 1000}]",NULL);
+	')
 	->toSucceed()
 )
 	
