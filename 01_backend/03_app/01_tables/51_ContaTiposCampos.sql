249704f1ac54efe3cdd9904e43fa34144011b184
Frederico <FredericoMFalcao@hotmail.com>
2020-10-01 16:18:44 +0100

    Create 51_ContaTiposCampos.sql

---
+++ 01_backend/03_app/01_tables/51_ContaTiposCampos.sql
@@ -0,0 +1,12 @@
+-- Tabela: ContaTipoCampos
+-- Descrição: Lista todos os campos e respectivos tipos para cada tipo de conta
+
+CREATE TABLE <?=tableNameWithModule()?> (
+  ContaTipo VARCHAR(255) NOT NULL,
+  Campo VARCHAR(255) NOT NULL, -- e.g. (Nome, Morada, NIF)
+  Tipo VARCHAR(255) NOT NULL,
+  ValorDefeito TEXT,
+  Opcoes JSON,
+  
+  FOREIGN KEY (ContaTipo) REFERENCES <?=tableNameWithModule("ContaTipos")?> (Tipo)
+);
