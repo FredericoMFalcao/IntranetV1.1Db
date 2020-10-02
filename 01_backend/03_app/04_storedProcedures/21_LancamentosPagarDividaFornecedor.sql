b81fef34969bb1c9ba4f2a9e49e0cb11b0829d16
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-02 15:47:49 +0100

    criei procedimento

--- 01_backend/03_app/04_storedProcedures/21_LancamentosPagarDividaFornecedor.sql
+++ 01_backend/03_app/04_storedProcedures/21_LancamentosPagarDividaFornecedor.sql
@@ -1,1 +1,36 @@
--- placeholder
+DROP PROCEDURE IF EXISTS LancamentosPagarDividaFornecedor;
+
+DELIMITER //
+
+CREATE PROCEDURE LancamentosPagarDividaFornecedor (IN in_NumSerie TEXT)
+ 
+  BEGIN
+           
+    -- 1. Inserir lançamentos na conta do fornecedor
+    CALL CriarLancamento (
+      JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.FornecedorCodigo'),
+      -1,
+      JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.PeriodoFaturacao'),
+      in_NumSerie
+    );
+    
+    
+    -- 2. Inserir lançamentos na conta bancária
+    CALL CriarLancamento (
+	  JSON_EXTRACT(
+	    (SELECT Extra
+	    FROM <?=tableNameWithModule("Documentos")?>
+	    WHERE Id = JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.ComprovativoPagamentoId')
+		),
+        '$.ContaBancaria'
+      ),
+      1,
+      JSON_EXTRACT((SELECT Extra FROM <?=tableNameWithModule("Documentos")?> WHERE NumSerie = in_NumSerie), '$.PeriodoFaturacao'),
+      in_NumSerie
+    );
+  
+  END;
+    
+//
+
+DELIMITER ;
