f49f04950943e18710aade6e10e24df4fe3a7f6e
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-02 13:00:09 +0100

    generalizado para todos os tipos de documento

--- 01_backend/03_app/04_storedProcedures/15_DocumentosCriar.sql
+++ 01_backend/03_app/04_storedProcedures/15_DocumentosCriar.sql
@@ -1,15 +1,15 @@
-DROP PROCEDURE IF EXISTS DocumentosCriarNovaFaturaFornecedor;
+DROP PROCEDURE IF EXISTS DocumentosCriar;
 
 DELIMITER //
 
--- Descrição: transforma um ficheiro no disco numa fatura de fornecedor por classificar
+-- Descrição: transforma um ficheiro no disco num documento
 --
 --        será chamada pelo sistema quando:
 --         (1) recebe um email com anexo PDF
 --         (2) ficheiro Dropbox
 --
 
-CREATE PROCEDURE DocumentosCriarNovaFaturaFornecedor (IN in_FileId TEXT)
+CREATE PROCEDURE DocumentosCriar (IN in_DocTipo TEXT, IN in_DocEstado TEXT, IN in_FileId TEXT)
   BEGIN
  
     -- 0. Verificar validade dos argumentos
@@ -19,7 +19,7 @@ CREATE PROCEDURE DocumentosCriarNovaFaturaFornecedor (IN in_FileId TEXT)
    
     -- 1. Inserir em Documentos 
     INSERT INTO <?=tableNameWithModule("Documentos")?> (Tipo, Estado, FileId) 
-    VALUES ('FaturaFornecedor', 'PorClassificarFornecedor', in_FileId);
+    VALUES ('in_DocTipo', 'in_DocEstado', in_FileId);
   
   END;
   
