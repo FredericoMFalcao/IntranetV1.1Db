232bc067db4358bcb5f00b5773685e254cefe88d
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-06 10:23:39 +0100

    acrescentei verificação de contas

--- 01_backend/03_app/04_storedProcedures/00_CriarLancamento.sql
+++ 01_backend/03_app/04_storedProcedures/00_CriarLancamento.sql
@@ -4,42 +4,62 @@ DELIMITER //
 
 -- Descrição: separa lancamentos por meses. 
 -- Função de suporte para outros stored procedures poderem criar linhas na tabela lançamentos. 
--- Cada linha é automaticamente desdobradas nas linhas necessárias para que fique uma linha cada mês incluído no período dado na chamada desta função.
+-- Cada linha é automaticamente desdobrada nas linhas necessárias para que fique uma linha por cada mês incluído no período dado na chamada desta função.
 
 CREATE PROCEDURE CriarLancamento (
-  Conta TEXT,
-  CoefRateio FLOAT,
-  Periodo TEXT, -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
-  DocNumSerie TEXT
+  in_Conta TEXT,
+  in_CoefRateio FLOAT,
+  in_Periodo TEXT, -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
+  in_DocNumSerie TEXT
   )
   BEGIN
     DECLARE d DATE;
     DECLARE n INT;
-    SET d = LAST_DAY(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Inicio'))) + INTERVAL 1 DAY - INTERVAL 1 MONTH;
+    DECLARE c INT;
+    DECLARE i INT;
+    SET d = LAST_DAY(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Inicio'))) + INTERVAL 1 DAY - INTERVAL 1 MONTH;
     -- d: primeiro dia do mês em que se inicia o período
-    SET n = MONTH(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim'))) - MONTH(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Inicio'))) + 1;
+    SET n = MONTH(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim'))) - MONTH(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Inicio'))) + 1;
     -- n: número de meses abrangidos pelo período
+    SET c = LENGTH(in_Conta) - LENGTH(REPLACE(in_Conta, ":", "")) + 1;
+    -- c: número de contas (para o caso de contas multi, e.g. 'CR0101:AN0202')
+    SET i = 1;
     
-    WHILE d <= JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim')) DO
+
+    -- 0. Verificar validade dos argumentos
+    WHILE i <= c DO
+  
+      IF NOT EXISTS (SELECT Conta FROM <?=tableNameWithModule("Contas")?> WHERE Conta = SUBSTRING_INDEX(SUBSTRING_INDEX(in_Conta, ":", i - c - 1), ":", 1))
+        THEN signal sqlstate '23000' set message_text = 'Conta inexistente.';
+      END IF;
+      
+      SET i = i + 1;
+    
+    END WHILE;
+      
+    
+    -- 1. Criar linhas na tabela de lançamentos
+    WHILE d <= JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim')) DO
     
       -- a) Caso em que o período é composto apenas por meses completos:
       IF (
-        DAY(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Inicio'))) = 1 AND
-        JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim')) = LAST_DAY(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim')))
+        DAY(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Inicio'))) = 1 AND
+        JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim')) = LAST_DAY(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim')))
         ) THEN
           INSERT INTO <?=tableNameWithModule("Lancamentos")?> (Conta, CoefRateio, Mes, DocNumSerie)
-          VALUES (Conta, CoefRateio / n, d, DocNumSerie);
+          VALUES (in_Conta, in_CoefRateio / n, d, in_DocNumSerie);
+          
       -- b) Caso em que o período começa e/ou acaba a meio de um mês:
       ELSE
         INSERT INTO <?=tableNameWithModule("Lancamentos")?> (Conta, CoefRateio, Mes, DocNumSerie)
         VALUES (
-          Conta,
-          CoefRateio * (
-            (DATEDIFF(LEAST(LAST_DAY(d), JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim'))), GREATEST(d, JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Inicio')))) + 1) /
-            (DATEDIFF(JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Fim')), JSON_UNQUOTE(JSON_EXTRACT(Periodo, '$.Inicio'))) + 1)
+          in_Conta,
+          in_CoefRateio * (
+            (DATEDIFF(LEAST(LAST_DAY(d), JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim'))), GREATEST(d, JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Inicio')))) + 1) /
+            (DATEDIFF(JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Fim')), JSON_UNQUOTE(JSON_EXTRACT(in_Periodo, '$.Inicio'))) + 1)
             ),
           d,
-          DocNumSerie
+          in_DocNumSerie
         );
       END IF;
     
