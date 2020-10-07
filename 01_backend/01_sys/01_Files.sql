08a29c78219de770f172c4a1023333eab4c881a7
Frederico <FredericoMFalcao@hotmail.com>
2020-10-07 18:54:01 +0100

    Improved way system handles Files

--- 01_backend/01_sys/01_Files.sql
+++ 01_backend/01_sys/01_Files.sql
@@ -1,4 +1,8 @@
 CREATE TABLE SYS_Files (
-  Id VARCHAR(255),
+  -- Id is the MD5 sum of the contents of the file
+  --    as produced by "md5sum" on ubuntu
+  Id CHAR(32),
+  MimeType VARCHAR(255) DEFAULT ("text/plain"),
+  DateCreated TIMESTAMP,
   PRIMARY KEY (Id)  
 );
