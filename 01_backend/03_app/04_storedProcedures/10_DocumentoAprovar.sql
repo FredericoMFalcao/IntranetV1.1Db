23f11f85286a5359633022aceac392350b7b2f42
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-08 12:49:42 +0100

    passa a receber com inputs apenas o Id e um JSON

--- 01_backend/03_app/04_storedProcedures/10_DocumentoAprovar.sql
+++ 01_backend/03_app/04_storedProcedures/10_DocumentoAprovar.sql
@@ -2,39 +2,56 @@ DROP PROCEDURE IF EXISTS DocumentoAprovar;
 
 DELIMITER //
 
-CREATE PROCEDURE DocumentoAprovar (
-  IN in_FaturaId                 INT,
-  
-  -- Inputs para classificar fornecedor:
-  IN in_NumSerie                 TEXT,
-  IN in_NumFatura                TEXT,
-  IN in_Projeto                  TEXT,
-  IN in_DataFatura               DATE,
-  IN in_DataRecebida             DATE,
-  IN in_PeriodoFaturacao         JSON,  -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-11-25"}
-  IN in_DataValidade             DATE,
-  IN in_FornecedorCodigo         TEXT,
-  IN in_Valor                    JSON,  -- e.g. {"Bens": {"ValorBase": 0.00, "Iva": 0.00}, "Servicos": {"ValorBase":0.00,"Iva":0.00}}
-  IN in_Moeda                    TEXT,
-  IN in_Descricao                TEXT,
-  IN in_ImpostoConsumo           DECIMAL(18,2),
-  IN in_Amortizacao              BOOLEAN,
-  
-  -- Inputs para classificar analítica:
-  IN in_ClassificacaoAnalitica   JSON,  -- e.g. [{"CentroResultados": "CR0101", "Analitica": "AN0202", "Colaborador": "COabc", "Valor": 1000}, {...}]
-  
-  -- Inputs para anexar comprovativo de pagamento:
-  IN in_ComprovativoPagamentoId   INT
-)
+CREATE PROCEDURE DocumentoAprovar (IN in_FaturaId INT, IN in_Extra JSON)
 
   BEGIN
+  
+    -- Inputs para classificar fornecedor:
+    DECLARE in_NumSerie                   TEXT;
+    DECLARE in_NumFatura                  TEXT;
+    DECLARE in_Projeto                    TEXT;
+    DECLARE in_DataFatura                 DATE;
+    DECLARE in_DataRecebida               DATE;
+    DECLARE in_PeriodoFaturacao           JSON;  -- e.g. {"Inicio": "2011-11-25", "Fim": "2011-12-25"}
+    DECLARE in_DataValidade               DATE;
+    DECLARE in_FornecedorCodigo           TEXT;
+    DECLARE in_Valor                      JSON;  -- e.g. {"Bens": {"ValorBase": 0.00, "Iva": 0.00}, "Servicos": {"ValorBase":0.00,"Iva":0.00}}
+    DECLARE in_Moeda                      TEXT;
+    DECLARE in_Descricao                  TEXT;
+    DECLARE in_ImpostoConsumo             DECIMAL(18,2);
+    DECLARE in_Amortizacao                BOOLEAN;
+    -- Inputs para classificar analítica:
+    DECLARE in_ClassificacaoAnalitica     JSON;  -- e.g. [{"CentroResultados": "CR0101", "Analitica": "AN0202", "Colaborador": "CO123", "Valor": 1000}, {...}]
+    -- Inputs para anexar comprovativo de pagamento:
+    DECLARE in_ComprovativoPagamentoId    INT;
+
+    -- Variável representativa do estado actual do documento
     DECLARE v_Estado TEXT;
+	
+	SET in_NumSerie = JSON_VALUE(in_Extra, '$.NumSerie');
+    SET in_NumFatura = JSON_VALUE(in_Extra, '$.NumFatura');
+    SET in_Projeto = JSON_VALUE(in_Extra, '$.Projeto');
+    SET in_DataFatura = JSON_VALUE(in_Extra, '$.DataFatura');
+    SET in_DataRecebida = JSON_VALUE(in_Extra, '$.DataRecebida');
+    SET in_PeriodoFaturacao = JSON_EXTRACT(in_Extra, '$.PeriodoFaturacao');
+    SET in_DataValidade = JSON_VALUE(in_Extra, '$.DataValidade');
+    SET in_FornecedorCodigo = JSON_VALUE(in_Extra, '$.FornecedorCodigo');
+    SET in_Valor = JSON_EXTRACT(in_Extra, '$.Valor');
+    SET in_Moeda = JSON_VALUE(in_Extra, '$.Moeda');
+    SET in_Descricao = JSON_VALUE(in_Extra, '$.Descricao');
+    SET in_ImpostoConsumo = JSON_VALUE(in_Extra, '$.ImpostoConsumo');
+    SET in_Amortizacao = JSON_VALUE(in_Extra, '$.Amortizacao');
+	SET in_ClassificacaoAnalitica = JSON_EXTRACT(in_Extra, '$.ClassificacaoAnalitica');
+	SET in_ComprovativoPagamentoId = JSON_VALUE(in_Extra, '$.ComprovativoPagamentoId');
+
     SET v_Estado = (SELECT Estado FROM <?=tableNameWithModule("Documentos")?> WHERE Id = in_FaturaId);
+
 
     -- 0. Verificar validade dos argumentos
     IF NOT EXISTS (SELECT Id FROM <?=tableNameWithModule("Documentos")?> WHERE Id = in_FaturaId AND Tipo = 'FaturaFornecedor')
       THEN signal sqlstate '20000' set message_text = 'Fatura de fornecedor inexistente.';
     END IF;
+
 
     -- 1. 'PorClassificarFornecedor' -> 'PorClassificarAnalitica'
     IF v_Estado = 'PorClassificarFornecedor' THEN
@@ -51,18 +68,16 @@ CREATE PROCEDURE DocumentoAprovar (
                 'ProjetoProvisorio', in_Projeto,
                 'DataFatura', in_DataFatura,
                 'DataRecebida', in_DataRecebida,
-
                 'DataValidade', in_DataValidade,
                 'FornecedorCodigo', in_FornecedorCodigo,
-
                 'Moeda', in_Moeda,
                 'Descricao', in_Descricao,
                 'ImpostoConsumo', in_ImpostoConsumo,
                 'Amortizacao', in_Amortizacao        
               ),
-              CONCAT("{\"PeriodoFaturacao\":", in_PeriodoFaturacao,"}")
+              CONCAT("{\"PeriodoFaturacao\":", in_PeriodoFaturacao, "}")
             ),
-            CONCAT("{\"Valor\":", in_Valor,"}")
+            CONCAT("{\"Valor\":", in_Valor, "}")
           )
         )
       WHERE Id = in_FaturaId;
@@ -71,7 +86,9 @@ CREATE PROCEDURE DocumentoAprovar (
     -- 2. 'PorClassificarAnalitica' -> 'PorRegistarContabilidade'
     ELSEIF v_Estado = 'PorClassificarAnalitica' THEN
       UPDATE <?=tableNameWithModule("Documentos")?> 
-      SET Estado = 'PorRegistarContabilidade'
+      SET
+	    Estado = 'PorRegistarContabilidade',
+		Extra = JSON_MERGE(Extra, CONCAT("{\"ClassificacaoAnalitica\":", in_ClassificacaoAnalitica, "}"))
       WHERE Id = in_FaturaId;
       
       -- Lançar dívida de fornecedor e custos:
