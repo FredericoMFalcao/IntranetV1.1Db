7967bed89600dabd2d7c23f0cf7ee56a4b80f5cf
Frederico <FredericoMFalcao@hotmail.com>
2020-10-08 12:08:57 +0100

    Sample "event system" code

--- 01_backend/01_sys/01_Files.sql
+++ 01_backend/01_sys/01_Files.sql
@@ -1,3 +1,8 @@
+-- ----------------
+--  1. Data Store
+--
+-- ----------------
+
 CREATE TABLE SYS_Files (
   -- Id is the MD5 sum of the contents of the file
   --    as produced by "md5sum" on ubuntu
@@ -6,3 +11,45 @@ CREATE TABLE SYS_Files (
   DateCreated TIMESTAMP,
   PRIMARY KEY (Id)  
 );
+
+-- ----------------
+--  2. C.U.D. Operations (with Events)
+--
+-- ----------------
+
+-- 2.1 Insert (with Events)
+DELIMITER //
+CREATE PROCEDURE FilesNew(Id CHAR(32), MimeType VARCHAR(255))
+BEGIN
+  
+  DECLARE i, j INT;
+  DECLARE funcName, sqlCode TEXT;
+                                                               
+  -- 2.1.1 Call BEFORE triggers
+  -- ---------------------------                                                               
+  SET i = 0; -- set counter to zero
+  -- calculate total number of "listening functions"
+  SET j = (SELECT COUNT(BeforeStoredProcedure) FROM SYS_EventHandlers WHERE EventName = "NewFile");
+  WHILE i != j DO
+    SELECT a.BeforeStoredProcedure INTO funcName FROM SYS_EventHandlers a LIMIT i,1;
+
+    -- Call funcName()
+    SET sqlCode = CONCAT('CALL ',funcName,'()'); PREPARE stmt1 FROM sqlCode; EXECUTE stmt1;
+
+    SET i = i + 1;
+  END WHILE;
+
+                                                               
+                                                               
+  -- 2.1.2 Perform operation
+  -- ---------------------------
+  INSERT INTO SYS_Files (Id, MimeType) VALUES (Id, MimeType);
+                                                               
+  -- 2.1.3 Call AFTER triggers
+  -- ---------------------------
+                                                               
+
+END;
+//
+
+DELIMITER ;
