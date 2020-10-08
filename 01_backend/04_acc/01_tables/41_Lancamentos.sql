d85acebbd38ee36496ce31592f8dfac79cb9cc11
Frederico <FredericoMFalcao@hotmail.com>
2020-10-08 15:27:55 +0100

    fixed typo

--- 01_backend/04_acc/01_tables/41_Lancamentos.sql
+++ 01_backend/04_acc/01_tables/41_Lancamentos.sql
@@ -8,5 +8,5 @@ CREATE TABLE <?=tableNameWithModule()?> (
   DocNumSerie  VARCHAR(20) NOT NULL, -- foreign key
   
   PRIMARY KEY (Conta,CoefRateio,Mes,DocNumSerie),
-  FOREIGN KEY (DocNumSerie) REFERENCES <?=tableNameWithModule("Documentos")?> (NumSerie) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (DocNumSerie) REFERENCES <?=tableNameWithModule("Documentos","DOC")?> (NumSerie) ON DELETE CASCADE ON UPDATE CASCADE
 );
