52749733f2777a570f3a8285bb7ab4be39a09ce3
Frederico <FredericoMFalcao@hotmail.com>
2020-10-09 15:25:47 +0100

    Adicionei comentário

--- 01_backend/03_doc/04_storedProcedures/14_DocumentoApagar.sql
+++ 01_backend/03_doc/04_storedProcedures/14_DocumentoApagar.sql
@@ -2,6 +2,13 @@ DROP PROCEDURE IF EXISTS DocumentoApagar;
 
 DELIMITER //
 
+-- ------------------------
+--  Tabela (sql): Documento Funcao: Apagar
+--
+-- Descrição: apaga um "documento", i.e. uma entrada na tabela sql de documentos
+--            Por consequência:
+--                (1) Chama o processo apropriado para cada tipo de documento
+-- ------------------------
 CREATE PROCEDURE DocumentoApagar (IN in_DocId INT)
 
   BEGIN
