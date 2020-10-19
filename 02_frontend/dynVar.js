6adbec22fa8c5245064c9d774125f136f3f57cf1
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-10-19 13:22:57 +0100

    updated the frontend code to handle partial and full updates on DOM elements

--- 02_frontend/dynVar.js
+++ 02_frontend/dynVar.js
@@ -2,21 +2,35 @@
 // @return Object
 // function dynVar() {
 	return {
-			"_value":"",
-			"get": function() { return this._value;}, 
-			"set": function(v){	this._value = v; for (k in this._listners) this._listners[k](v); return this;},
-			"_listners":{},
-			"addListener": function (fn,obj){
-				// Generate unique/not-used key for _listners object
-				do {key = Math.floor(Math.random()*1000).toString(16);} 
-				while (typeof this._listners[key] != "undefined");
-				// Add the function
-				this._listners[key] = fn.bind(obj);
-				// return the key associated, useful to remove the listener
-				return key;
-			},
-			"removeListener": function(key) {
-				delete this._listners[key];
-			}
-		};
+				"_value":"",
+				"get": function() { return this._value;}, 
+				"clear": function(){ this._value = ""; },
+				"set": function(v){
+					// If the new value (v) to be assigned is an object
+					// simply merge properties
+					if (typeof this._value == "object" && typeof v == "object")
+						Object.assign(this._value, v);
+					else
+						this._value = v;
+					// Calls all the listeners expecting new events with :
+					//   1. the object provided at the time of the call "addListener"
+					//   2. the new value
+					for (k in this._listners) 
+						this._listners[k][0](this._listners[k][1],this._value);
+					return this;
+				},
+				"_listners":{},
+				"addListener": function (fn,obj){
+					// Generate unique/not-used key for _listners object
+					do {key = Math.floor(Math.random()*1000).toString(16);} 
+					while (typeof this._listners[key] != "undefined");
+					// Add the function
+					this._listners[key] = [fn,obj];
+					// return the key associated, useful to remove the listener
+					return key;
+				},
+				"removeListener": function(key) {
+					delete this._listners[key];
+				}
+			};
 // }
--- 02_frontend/view.js
+++ 02_frontend/view.js
@@ -1,10 +1,26 @@
-// @Description: class view creator. A view is object that renders itself as DOM Element, reacts to data changed, and notifies of internal events
+// @Description: a base view's contructor expects:
+//			1. an initial data object
+//			2. a full render callback function that receives:
+//				2.1 the top "dom_el" to overwrite
+//				2.2 a list of arguments with static data
+//			3. a listening callback function that reacts to partial data updates. It expects:
+//				3.1 the top "dom_el" to manipulate
+//				3.2 the variable that changed and triggered this function
+//			4. an array of event names to capture
+//			5. a listening callback function that reacts to events
+//
+//
 // @param DomElementStr string the type of HTML tag for top wrapping element
 // @param dynVars object an array of dynamic variables
 // @param updateContentFn function a callback to be called when the underlying data (from dynVars) changes
 // @param handleEventFn function? a callback to be called when some internal events (keypress, mouseclick, ...) happens
 // @return null
-//function view(DomElementStr, dynVars, updateContentFn, handleEventFn) {
+// function view(DomElementStr, dynVars, updateContentFn, handleEventFn) {
+	/* 0. Check types */
+	if (typeof DomElementStr != "string") throw "DomElementStr expected to be string";
+	if (typeof dynVars != "object") throw "dynVars expected to be array";
+	if (typeof updateContentFn != "function") throw "updateContentFn expected to be function";
+	if (!["function","undefined"].includes(typeof handleEventFn)) throw "handleEventFn expected to be function or undefined";		
 	
 	/* 1. Create the DOM element */
 	var dom_el = document.createElement(DomElementStr);
@@ -20,7 +36,7 @@
 			listening_keys.push(dynVars[k].removeListener(dom_el.listening_keys));			
 	}
 	/* 5. Force an initial update */
-	dom_el.updateData();
+	dom_el.updateData(dom_el);
 	
 	/* 6. Listen to dom element events, update variable(s) */		
 	if (typeof handleEventFn != "undefined")
@@ -28,4 +44,5 @@
 					
 	/* 10. Return Dom Element */
 	return dom_el						
-//}
+// }
+
