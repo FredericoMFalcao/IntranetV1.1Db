375928096142325159ffe78603c47a72132d95b8
Frederico <FredericoMFalcao@hotmail.com>
2020-10-19 13:26:39 +0100

    Improved comments

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,25 @@
 <html>
 <head>
+<?php 
+     /* 1. APP's EXTERNAL JAVASCRIPT LIBs / CSS Stylesheets
+     *    
+     *   Include all the Apps requires external assets (js, css)
+     */
+?>
+	
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 <!--	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.3/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> -->
-<script>
-<?php 
 
+<?php 
+     /* 2. APP's CUSTOM JAVASCRIPT FUNCTIONS
+     *    
+     *   Include all the Apps global script functions defined in the SQL database 
+     */
+     echo "<script>";
       require_once __DIR__."/../db_support_functions.php";
       if(($js_funcs = sql("SELECT FuncName, InputArgs_json, Code  FROM PLT_GUI_Javascript")) !== false)  
 	foreach($js_funcs as $js_func) {
@@ -27,9 +38,9 @@
 		echo $Code;
 		echo "\n}\n";
 	}
-	
+     echo "</script>";
 ?>
-</script>
+
 </head>
 <body>
 
