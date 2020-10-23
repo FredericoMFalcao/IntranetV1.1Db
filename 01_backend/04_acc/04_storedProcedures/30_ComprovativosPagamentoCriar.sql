8e6f604cc5331241377d106cc44a91756eac1f31
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-23 12:07:21 +0100

    Passa a inserir o 'NumSerie' na tabela de documentos

--- 01_backend/04_acc/04_storedProcedures/30_ComprovativosPagamentoCriar.sql
+++ 01_backend/04_acc/04_storedProcedures/30_ComprovativosPagamentoCriar.sql
@@ -9,16 +9,18 @@ DELIMITER //
 -- ------------------------
 CREATE PROCEDURE ComprovativosPagamentoCriar (IN in_Extra JSON)
   BEGIN
+    DECLARE in_NumSerie TEXT;
     DECLARE in_DocTipo TEXT;
     DECLARE in_FileId TEXT;
     DECLARE in_ContaBancaria TEXT;
+    SET in_NumSerie = JSON_VALUE(in_Extra, '$.NumSerie');
     SET in_DocTipo = JSON_VALUE(in_Extra, '$.DocTipo');
     SET in_FileId = JSON_VALUE(in_Extra, '$.FileId');
     SET in_ContaBancaria = JSON_VALUE(in_Extra, '$.ContaBancaria');
 
     IF in_DocTipo = 'ComprovativoPagamento' THEN
-      INSERT INTO <?=tableNameWithModule("Documentos","DOC")?> (Tipo, Estado, FileId) 
-      VALUES (in_DocTipo, 'Concluido', in_FileId);
+      INSERT INTO <?=tableNameWithModule("Documentos","DOC")?> (NumSerie, Tipo, Estado, FileId) 
+      VALUES (in_NumSerie, in_DocTipo, 'Concluido', in_FileId);
       UPDATE <?=tableNameWithModule("Documentos","DOC")?>
       SET Extra = JSON_SET(Extra, '$.ContaBancaria', in_ContaBancaria)
       WHERE FileId = in_FileId;
