144c6d7ce2b48a486ecacdc7256fbd71d55fc194
Frederico <FredericoMFalcao@hotmail.com>
2020-10-27 11:24:05 +0000

    Added Event Triggering Feature

--- 01_backend/01_sys/02_EventHandlers.sql
+++ 01_backend/01_sys/02_EventHandlers.sql
@@ -9,3 +9,31 @@ CREATE TABLE SYS_EventHandlers (
   AfterStoredProcedure VARCHAR(255) NOT NULL,
   FOREIGN KEY (EventName) REFERENCES SYS_Events(Name)
 );
+
+<?php foreach(["Before", "After"] as $Priority) : ?>
+DROP PROCEDURE IF EXISTS SYS_Trigger<?=$Priority?>Event;
+DELIMITER //
+CREATE PROCEDURE SYS_Trigger<?=$Priority?>Event (
+  IN in_EventName VARCHAR(255), 
+  IN in_Options   JSON
+  )
+BEGIN
+    DECLARE i,j INT; -- counter needed to trigger MANY (not single) events
+	  SET i = 0; -- set counter to zero
+
+    -- calculate total number of "listening functions"
+	  SET j = (SELECT COUNT(<?=$Priority?>StoredProcedure) FROM SYS_EventHandlers WHERE EventName = in_EventName AND <?=$Priority?>StoredProcedure IS NOT NULL);
+	  WHILE i != j DO
+      -- store current procedure name into variable: funcName
+	    SELECT a.<?=$Priority?>StoredProcedure INTO funcName FROM SYS_EventHandlers a AND a.<?=$Priority?>StoredProcedure IS NOT NULL LIMIT i,1;
+
+	    -- Call funcName()
+	    SET sqlCode = CONCAT('CALL ',funcName,'(?)'); PREPARE stmt1 FROM sqlCode; EXECUTE stmt1 USING in_Options; DEALLOCATE PREPARE stmt1; 
+
+	    SET i = i + 1; -- increment counter
+	  END WHILE;
+    
+END;
+//  
+DELIMITER ;
+<?php endforeach; ?>
