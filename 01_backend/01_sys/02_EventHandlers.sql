7755a3b6126fe021279f1f6f6fcbcc170f0f1ac9
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-27 12:22:17 +0000

    Trigger fields can be NULL

--- 01_backend/01_sys/02_EventHandlers.sql
+++ 01_backend/01_sys/02_EventHandlers.sql
@@ -5,8 +5,8 @@
 -- -------------
 CREATE TABLE SYS_EventHandlers (
   EventName VARCHAR(255) NOT NULL,
-  BeforeStoredProcedure VARCHAR(255) NOT NULL,
-  AfterStoredProcedure VARCHAR(255) NOT NULL,
+  BeforeStoredProcedure VARCHAR(255),
+  AfterStoredProcedure VARCHAR(255),
   FOREIGN KEY (EventName) REFERENCES SYS_Events(Name)
 );
 
