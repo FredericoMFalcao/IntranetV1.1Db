913d9a7588e5d337615e168912c0feafb60f3351
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-10-29 14:22:14 +0000

    added backend script to execute raw sql queries

--- public_html/sql.py
+++ public_html/sql.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 from project_root import db_support_functions as db
+import cgi
 import cgitb
 import json
 cgitb.enable()    
 print("Content-Type: text/html;charset=utf-8")
 print("")
 
-print(json.dumps(db.sql("SELECT * FROM DocEstados")))
+get_arguments = cgi.FieldStorage()
+if "q" in get_arguments :
+	print json.dumps(db.sql(get_arguments["q"].value))
+else :
+	print "ERROR: No query provided."
