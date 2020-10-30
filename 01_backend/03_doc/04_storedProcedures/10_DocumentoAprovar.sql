35d9d2bded33d2129e8969d7e18ef3a0d520a469
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-30 08:02:24 +0000

    fixed indentation

--- 01_backend/03_doc/04_storedProcedures/10_DocumentoAprovar.sql
+++ 01_backend/03_doc/04_storedProcedures/10_DocumentoAprovar.sql
@@ -12,24 +12,24 @@ CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_DocId INT, IN in_Arguments JS
 
   BEGIN
     DECLARE v_Estado TEXT;
-	SET v_Estado = (SELECT Estado FROM <?=tableNameWithModule("Documentos")?> WHERE Id = in_DocId);
+    SET v_Estado = (SELECT Estado FROM <?=tableNameWithModule("Documentos")?> WHERE Id = in_DocId);
   
     -- 1. Alterar estado do documento
     -- --------------------------
     
     -- 1.1 Espoletar evento BEFORE
-	CALL <?=tableNameWithModule("TriggerBeforeEvent","SYS")?> ("DocumentoAprovado",JSON_OBJECT("DocId", in_DocId, "Extra", in_Arguments));
-	
-	-- 1.2 Executar acção
-	IF v_Estado = 'PorClassificarFornecedor' THEN
+    CALL <?=tableNameWithModule("TriggerBeforeEvent","SYS")?> ("DocumentoAprovado",JSON_OBJECT("DocId", in_DocId, "Extra", in_Arguments));
+    
+    -- 1.2 Executar acção
+    IF v_Estado = 'PorClassificarFornecedor' THEN
       UPDATE <?=tableNameWithModule("Documentos")?> 
       SET Estado = 'PorClassificarAnalitica'
-	  WHERE Id = in_DocId;
+      WHERE Id = in_DocId;
 
     ELSEIF v_Estado = 'PorClassificarAnalitica' THEN
       UPDATE <?=tableNameWithModule("Documentos")?> 
       SET Estado = 'PorRegistarContabilidade'
-	  WHERE Id = in_DocId;
+      WHERE Id = in_DocId;
 
     ELSEIF v_Estado = 'PorRegistarContabilidade' THEN
       UPDATE <?=tableNameWithModule("Documentos")?> 
@@ -45,11 +45,11 @@ CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_DocId INT, IN in_Arguments JS
       UPDATE <?=tableNameWithModule("Documentos")?>  
       SET Estado = 'Concluido'
       WHERE Id = in_DocId;
-	  
-	END IF;
-	
-	-- 1.3 Espoletar evento AFTER
-	CALL <?=tableNameWithModule("TriggerAfterEvent","SYS")?> ("DocumentoAprovado",JSON_OBJECT("DocId", in_DocId, "Arguments", in_Arguments));
+      
+    END IF;
+    
+    -- 1.3 Espoletar evento AFTER
+    CALL <?=tableNameWithModule("TriggerAfterEvent","SYS")?> ("DocumentoAprovado",JSON_OBJECT("DocId", in_DocId, "Arguments", in_Arguments));
 
   END;
   
