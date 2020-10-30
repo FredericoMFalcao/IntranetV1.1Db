0d8c7ac9706330ce520b4d5b747d45eb97da464a
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-10-30 08:52:51 +0000

    fixed indentation

--- 01_backend/04_acc/06_eventHandlers/02_onBeforeDocumentoAprovado.sql
+++ 01_backend/04_acc/06_eventHandlers/02_onBeforeDocumentoAprovado.sql
@@ -11,11 +11,11 @@ DELIMITER //
 CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_Options JSON)
   BEGIN
     DECLARE in_DocId INT;
-	DECLARE in_Extra JSON;
+    DECLARE in_Extra JSON;
     DECLARE v_DocTipo TEXT;
-	
-	SET in_DocId = JSON_VALUE(in_Options, '$.DocId');
-	SET in_Extra = JSON_EXTRACT(in_Options, '$.Arguments');
+    
+    SET in_DocId = JSON_VALUE(in_Options, '$.DocId');
+    SET in_Extra = JSON_EXTRACT(in_Options, '$.Arguments');
     SET v_DocTipo = (SELECT Tipo FROM <?=tableNameWithModule("Documentos","DOC")?> WHERE Id = in_DocId);
    
     IF v_DocTipo = "FaturaFornecedor" THEN
