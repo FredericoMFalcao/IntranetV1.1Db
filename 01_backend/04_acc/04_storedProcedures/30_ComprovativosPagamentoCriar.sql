478edc2b575e747ec201af977646704146f716c6
antoniomribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-11-19 10:20:02 +0000

    Criado campo 'DataPagamento'

--- 01_backend/04_acc/04_storedProcedures/30_ComprovativosPagamentoCriar.sql
+++ 01_backend/04_acc/04_storedProcedures/30_ComprovativosPagamentoCriar.sql
@@ -13,15 +13,17 @@ CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_Arguments JSON)
     DECLARE in_DocId TEXT;
     DECLARE in_NumSerie TEXT; -- TEMPORÁRIO: falta definir em que fase é atribuído um 'NumSerie' ao comprovativo de pagamento
     DECLARE in_ContaBancaria TEXT;
+    DECLARE in_DataPagamento DATE;
     SET in_DocId = JSON_VALUE(in_Arguments, '$.Id');
     SET in_NumSerie = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.NumSerie');
     SET in_ContaBancaria = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.ContaBancaria');
+    SET in_DataPagamento = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.DataPagamento');
 
     UPDATE <?=tableNameWithModule("Documentos","DOC")?>
     SET 
       Tipo = 'ComprovativoPagamento', 
       Estado = 'Concluido',
-      Extra = JSON_SET(Extra, '$.ContaBancaria', in_ContaBancaria)
+      Extra = JSON_SET(Extra, '$.ContaBancaria', in_ContaBancaria, '$.DataPagamento', in_DataPagamento)
     WHERE Id = in_DocId;
   
   END;
