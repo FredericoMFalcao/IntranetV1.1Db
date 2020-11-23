03300178048a77dbe384875ebecc10e8eae9af00
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-11-23 18:18:12 +0000

    fixed inputs

--- 01_backend/04_acc/04_storedProcedures/32_ComprovativoPagamentoAprovar.sql
+++ 01_backend/04_acc/04_storedProcedures/32_ComprovativoPagamentoAprovar.sql
@@ -3,27 +3,25 @@ DROP PROCEDURE IF EXISTS <?=tableNameWithModule()?>;
 DELIMITER //
 
 -- ------------------------
---  Tabela (virtual): ComprovativosPagamento Funcao: Aprovar 
+-- Tabela (virtual): ComprovativosPagamento Funcao: Aprovar 
 --
 -- Descrição: antes de um comprovativo de pagamento ser aprovado, acrescenta informação específica ao documento
 -- ------------------------
 
-CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_Arguments JSON)
+CREATE PROCEDURE <?=tableNameWithModule()?> (IN in_ComprovativoPagamentoId INT, IN in_Extra JSON)
   BEGIN
-    DECLARE in_DocId TEXT;
     DECLARE in_NumSerie TEXT;
     DECLARE in_ContaBancaria TEXT;
     DECLARE in_DataPagamento DATE;
-    SET in_DocId = JSON_VALUE(in_Arguments, '$.Id');
-    SET in_NumSerie = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.NumSerie');
-    SET in_ContaBancaria = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.ContaBancaria');
-    SET in_DataPagamento = JSON_VALUE(JSON_EXTRACT(in_Arguments, '$.Extra'), '$.DataPagamento');
+    SET in_NumSerie = JSON_VALUE(JSON_EXTRACT(in_Extra, '$.Extra'), '$.NumSerie');
+    SET in_ContaBancaria = JSON_VALUE(JSON_EXTRACT(in_Extra, '$.Extra'), '$.ContaBancaria');
+    SET in_DataPagamento = JSON_VALUE(JSON_EXTRACT(in_Extra, '$.Extra'), '$.DataPagamento');
 
     UPDATE <?=tableNameWithModule("Documentos","DOC")?>
     SET 
       NumSerie = in_NumSerie,
       Extra = JSON_SET(Extra, '$.ContaBancaria', in_ContaBancaria, '$.DataPagamento', in_DataPagamento)
-    WHERE Id = in_DocId;
+    WHERE Id = in_ComprovativoPagamentoId;
   
   END;
   
