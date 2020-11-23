3897458a53b627d0805daa7ade72e83ad3f9ecfd
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-11-23 16:05:58 +0000

    added working feature: force code deployment to last_build panel

--- public_html/last_build.php
+++ public_html/last_build.php
@@ -1,10 +1,14 @@
+<?php if ($_GET['createNewBranchWithName']) {$output = json_encode(shell_exec(__DIR__."/../../fork.sh ".$_GET['createNewBranchWithName']) ?? "success"); die("alert('{$_GET['createNewBranchWithName']} branch created ($output)}');");} ?>
+<?php if ($_GET['deleteThisBranch']) {$output = json_encode(shell_exec(__DIR__."/../../deleteThisBranch.sh ") ?? "success"); die("alert('Branch deleted ($output)}');");} ?>
+<?php if ($_GET['forceCodeDeployment']) {$output = json_encode(shell_exec("FORCE_DEPLOY=1 ".__DIR__."/../deploy_code.sh ") ?? "success"); die("alert('Success! ($output)}');");} ?>
+
 <html>
 	<head>
 		<!-- BOOTSTRAP LIBRARIES -->
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 		
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
@@ -20,9 +24,10 @@
 		<h2>Links</h2>
 			<ul class="list-group list-group-horizontal">
 				<li class="list-group-item">	<a class="btn btn-default" href="sql_explorer.php">sql explorer</a></li>
-				<li class="list-group-item">	<a class="btn btn-danger" href="deploy_code.sh">force code deployment</a></li>
+				<li class="list-group-item">	<button class="btn btn-danger" onclick="$.ajax({url:'',data:{deleteThisBranch:confirm('Are you sure?')},success:function(data){eval(data);}});">Delete this branch</button></li>
+				<li class="list-group-item">	<button class="btn btn-primary" onclick="$.ajax({url:'',data:{forceCodeDeployment:1},success:function(data){eval(data);}});">Force Code Deployment</button></li>
 				<li class="list-group-item">	<button class="btn btn-primary" onclick="alert('not yet implemented');">Merge Branch Into Master</button></li>
-				<li class="list-group-item">	<button class="btn btn-primary" onclick="alert('not yet implemented');">Fork</button></li>
+				<li class="list-group-item">	<button class="btn btn-primary" onclick="$.ajax({url:'',data:{createNewBranchWithName:prompt('New Branch Name?')},success:function(data){eval(data);}});">Fork</button></li>
 			</ul>
 		<hr>
 		
