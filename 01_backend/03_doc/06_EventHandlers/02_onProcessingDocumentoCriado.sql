7729c13cb9ebe0980ee64ca7e3da843bf1e59cd5
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2020-11-24 11:40:22 +0000

    passa a receber apenas um argumento (JSON)

--- 01_backend/03_doc/06_EventHandlers/02_onProcessingDocumentoCriado.sql
+++ 01_backend/03_doc/06_EventHandlers/02_onProcessingDocumentoCriado.sql
@@ -6,9 +6,13 @@
 
 DELIMITER //
 
-CREATE OR REPLACE PROCEDURE <?=tableNameWithModule()?> (IN in_FileId TEXT, IN in_Extra JSON)
+CREATE OR REPLACE PROCEDURE <?=tableNameWithModule()?> (IN in_Options TEXT)
 
   BEGIN
+    DECLARE in_FileId TEXT;
+    DECLARE in_Extra JSON;
+    SET in_FileId = JSON_VALUE(in_Options, '$.FileId');
+    SET in_Extra = JSON_EXTRACT(in_Options, '$.Extra');
 
     INSERT INTO <?=tableNameWithModule("Documentos","DOC")?> (FileId, Extra) VALUES (in_FileId, in_Extra);
       
