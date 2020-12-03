1b297de90c417eb923c0e10f3b90b61bb6f29ef5
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-12-03 14:12:49 +0000

    fixed data type.

--- 01_backend/02_plt/01_tables/30_CalendarActions.sql
+++ 01_backend/02_plt/01_tables/30_CalendarActions.sql
@@ -1,8 +1,11 @@
 -- Table: CalendarActions
 -- Description: Performs one-off or regular actions
 --              described in SQL code or PHP code
---              1. SQL code is executes with admin priviliges
+--              1. SQL code is executes with webServer username and priviliges
+--                  @todo: create a custom sql username for calendar actions
 --              2. PHP code doesn't need the start tag "< ? php"
+--                  - executes with "www-data" (apache's user)
+--                  - has sql() function available to perform actions on the database
 
 CREATE TABLE <?=tableNameWithModule()?> (
    id                     INT PRIMARY KEY AUTO_INCREMENT,
@@ -12,8 +15,8 @@ CREATE TABLE <?=tableNameWithModule()?> (
 
 -- ACTIVE: boolean (true/false) bit useful for already performed actions, report status
    Active                 INT DEFAULT(1),
-   LastActionDate         DATE,
-   NextActionDate         DATE NOT NULL,
+   LastActionDate         DATETIME,
+   NextActionDate         DATETIME NOT NULL,
    RepeatEveryNSeconds    INT,
    SqlCode                TEXT,
    PhpCode                TEXT,
