b6c9212d3ce6ddb8c136e2295386c69ba20e8f81
Frederico Falcao <ubuntu@ip-172-31-9-4.eu-west-1.compute.internal>
2020-12-03 14:20:43 +0000

    added feature to perform recurring events

--- 01_backend/02_plt/10_cronScripts/performActions.php
+++ 01_backend/02_plt/10_cronScripts/performActions.php
@@ -3,18 +3,25 @@
 require_once __DIR__."/../../../db_support_functions.php";
 
 $sqlQuery  = "SELECT ";
-$sqlQuery .= "id, SqlCode, PhpCode ";
+$sqlQuery .= "id, SqlCode, PhpCode, RepeatEveryNSeconds ";
 $sqlQuery .= "FROM PLT_CalendarActions ";
 $sqlQuery .= "WHERE Active = 1 AND ";
 $sqlQuery .= "NextActiveDate - NOW() > 0  ";
 
+/*
+*	MAIN LOOP: execute each actions
+*/
 foreach(sql($sqlQuery) as $row) {
 	extract($row);
+
+	// Update the NextActiveDate (if RepeatEveryNSeconds exists)
+	sql("UPDATE PLT_CalendarActions SET NextActiveDate = FROM_UNIXTIME(UNIX_TIMESTAMP(NOW()+RepeatEveryNSeconds)) WHERE id = $id");
+
 	if ($SqlCode) {
 		$errorInfo = [];
 		if (!sql($SqlCode, $errorInfo))
 			// Log the Error return value
-			sql("UPDATE PLT_CalendarActions SET Log = '".str_replace("'", '"', print_r($errorInfo,1))."' WHERE id = $id");  continue; }
+		{	sql("UPDATE PLT_CalendarActions SET Log = '".str_replace("'", '"', print_r($errorInfo,1))."' WHERE id = $id");  continue; }
 		else
 			// Log the SUCCESS return value
 			sql("UPDATE PLT_CalendarActions SET Log = 'Success' WHERE id = $id");
@@ -28,4 +35,6 @@ foreach(sql($sqlQuery) as $row) {
 		// Log the SUCCESS return value
 		sql("UPDATE PLT_CalendarActions SET Log = '".str_replace("'", '"', $output)."' WHERE id = $id");
 	}
+
+
 }
