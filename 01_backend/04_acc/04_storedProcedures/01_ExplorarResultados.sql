5272da80491b269396555d0f07f51bcb0159bbf1
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2021-01-06 17:53:02 +0000

    separei lógicas de resultados e tesouraria

--- 01_backend/04_acc/04_storedProcedures/01_ExplorarResultados.sql
+++ 01_backend/04_acc/04_storedProcedures/01_ExplorarResultados.sql
@@ -2,53 +2,75 @@
 -- Explorador de Resultados
 --
 -- Descrição: devolve os movimentos contabilísticos agregados mensalmente, dados:
---              - o tipo de movimento (e.g. custos, proveitos, resutados, saldo)
+--              - o tipo de movimento (e.g. custos, proveitos, resutados)
 --              - a data do movimento (e.g. emissão da fatura, período faturado, data de pagamento)
---              - um agregador (e.g. centros de resultados, contas analíticas, um fornecedor específico)
---              - o ano pretendido
+--              - um agregador (e.g. centros de resultados, contas analíticas, colaboradores, fornecedores, clientes)
+--              - o período pretendido
 -- ------------------------
 
 DELIMITER //
 
-CREATE OR REPLACE PROCEDURE <?=tableNameWithModule()?> (
-  in_Visao CHAR(1),   -- "F": Faturação, "G": Gestão, "T": Tesouraria
-  in_Agregador TEXT,  -- e.g. "CR", "AN", "CO", "FO", "CR01", "AN0202", "CO123", "FO0000111"
-  in_Ano INT
-  -- in_TipoMovimento TEXT,  -- Resultados, Custos, Proveitos
-  -- in_Moeda CHAR(3)
-)
+CREATE OR REPLACE PROCEDURE <?=tableNameWithModule()?> (in_Arguments JSON)
 
   BEGIN
-
-    SELECT
-      CASE
-        WHEN LEFT(in_Agregador, 2) IN ("CR", "AN")
-          THEN LEFT(ACC_ExtrairConta(LEFT(in_Agregador, 2), a.Conta), LENGTH(in_Agregador) + 2)
-        WHEN LEFT(in_Agregador, 2) = "CO"
-          THEN ACC_ExtrairConta("CO", a.Conta)
-        WHEN LEFT(in_Agregador, 2) = "FO"
-          THEN a.Conta
-      END AS Agregador,
-      CASE in_Visao
-        WHEN "F" THEN EXTRACT(YEAR_MONTH FROM JSON_VALUE(b.Extra, '$.DataFatura'))
-        WHEN "G" THEN EXTRACT(YEAR_MONTH FROM a.Mes)
-        WHEN "T" THEN EXTRACT(YEAR_MONTH FROM (JSON_VALUE(c.Extra, '$.DataPagamento')))
-      END AS Periodo,
-      CASE
-        WHEN LEFT(in_Agregador, 2) = "FO"
-          THEN CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra) * (-1)) AS DECIMAL(18, 2))
-        ELSE CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra)) AS DECIMAL(18, 2))
-      END AS Valor
-    FROM <?=tableNameWithModule("Lancamentos")?> AS a
-      LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS b ON a.DocNumSerie = b.NumSerie
-      LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS c ON JSON_VALUE(b.Extra, '$.ComprovativoPagamentoId') = c.Id
-    WHERE CASE WHEN LEFT(in_Agregador, 2) = "FO" THEN a.CoefRateio > 0 ELSE 1 END
-    GROUP BY Agregador, Periodo
-    HAVING 
-      LEFT(Periodo, 4) = in_Ano AND
-      LEFT(Agregador, LENGTH(in_Agregador)) = in_Agregador
-    ORDER BY Agregador, Periodo;
+  
+    DECLARE in_Agregador TEXT;     -- "CR", "AN", "CO", "FO", "CL", "CR01", "AN0202", "CO123", "FO0000111", "CL001", etc
+    DECLARE in_Visao CHAR(1);      -- "F": Faturação, "G": Gestão, "T": Tesouraria
+    DECLARE in_Movimento CHAR(1);  -- "C": Custos, "P": Proveitos, "R": Resultados
+    DECLARE in_Ano INT;
+    -- in_Moeda CHAR(3)
     
+    SET in_Agregador = JSON_VALUE(in_Arguments, '$.Agregador');
+    SET in_Visao = JSON_VALUE(in_Arguments, '$.Visao');
+    SET in_Movimento = JSON_VALUE(in_Arguments, '$.Movimento');
+    SET in_Ano = JSON_VALUE(in_Arguments, '$.Ano');
+    
+    
+    -- -------
+    -- 1. Visão: F / G (Agregador: CR / AN / CO, Movimento: C / P / R, Ano: 20XX)
+    -- -------
+    IF in_Visao IN ("F", "G") THEN
+  
+      SELECT
+        CASE
+          WHEN LEFT(in_Agregador, 2) = "CO" THEN ACC_ExtrairConta("CO", a.Conta)
+          ELSE LEFT(ACC_ExtrairConta(LEFT(in_Agregador, 2), a.Conta), LENGTH(in_Agregador) + 2)
+        END AS Agregador,
+        CASE in_Visao
+          WHEN "F" THEN EXTRACT(YEAR_MONTH FROM JSON_VALUE(b.Extra, '$.DataFatura'))
+          WHEN "G" THEN EXTRACT(YEAR_MONTH FROM a.Mes)
+        END AS Periodo,
+        CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra)) AS DECIMAL(18, 2)) AS Valor
+      FROM <?=tableNameWithModule("Lancamentos")?> AS a
+        LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS b ON a.DocNumSerie = b.NumSerie
+      WHERE
+        CASE
+          WHEN in_Movimento = "C" THEN a.CoefRateio < 0
+          WHEN in_Movimento = "P" THEN a.CoefRateio > 0
+        ELSE 1 END
+      GROUP BY Agregador, Periodo
+      HAVING 
+        LEFT(Periodo, 4) = in_Ano AND
+        LEFT(Agregador, LENGTH(in_Agregador)) = in_Agregador
+      ORDER BY Agregador, Periodo;
+      
+      
+    -- -------
+    -- 2. Visão: T (Agregador: FO / CL, Movimento: -, Ano: -)
+    -- -------
+    ELSEIF in_Visao = "T" THEN
+    
+      SELECT
+        a.Conta AS Agregador,
+        CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra)) AS DECIMAL(18, 2)) AS ValorEmDivida
+      FROM <?=tableNameWithModule("Lancamentos")?> AS a
+        LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS b ON a.DocNumSerie = b.NumSerie
+      WHERE LEFT(a.Conta, LENGTH(in_Agregador)) = in_Agregador
+      GROUP BY Agregador
+      ORDER BY Agregador;
+      
+    END IF;
+  
   END;
   
 //
