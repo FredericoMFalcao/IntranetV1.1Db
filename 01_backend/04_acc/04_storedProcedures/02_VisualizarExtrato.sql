331e1018cc6fca4edbfcf883fc0a0eeb227ccab4
António Melo Ribeiro <70759109+antoniomribeiro@users.noreply.github.com>
2021-01-08 10:40:18 +0000

    separei lógicas de resultados e tesouraria

--- 01_backend/04_acc/04_storedProcedures/02_VisualizarExtrato.sql
+++ 01_backend/04_acc/04_storedProcedures/02_VisualizarExtrato.sql
@@ -7,33 +7,70 @@
 
 DELIMITER //
 
-CREATE OR REPLACE PROCEDURE <?=tableNameWithModule()?> (
-  in_Visao CHAR(1),  -- "F": Faturação, "G": Gestão, "T": Tesouraria
-  in_Conta TEXT,     -- e.g. "CR01", "AN0202", "CO123", "FO0000111"
-  in_AnoMes INT      -- e.g. 201804
-)
-
+CREATE OR REPLACE PROCEDURE ACC_VisualizarExtrato (in_Arguments JSON)
+  
   BEGIN
+  
+    -- Inputs obrigatórios:
+    DECLARE in_Agregador TEXT;     -- "CR01", "AN0202", "CO123", "FO0000111", "CL001", etc
+    DECLARE in_Visao CHAR(1);      -- "F": Faturação, "G": Gestão, "T": Tesouraria
+    
+    -- Inputs exclusivos às visões F e G:
+    DECLARE in_Movimento CHAR(1);  -- "C": Custos, "P": Proveitos, "R": Resultados
+    DECLARE in_AnoMes INT;         -- e.g. 201804
+    
+    SET in_Agregador = JSON_VALUE(in_Arguments, '$.Agregador');
+    SET in_Visao = JSON_VALUE(in_Arguments, '$.Visao');
 
+
+    -- -------
+    -- 1. Visão: F / G (Agregador: CR / AN / CO, Movimento: C / P / R, AnoMes: 20XXXX)
+    -- -------
+    IF in_Visao IN ("F", "G") THEN
+    
+    SET in_Movimento = JSON_VALUE(in_Arguments, '$.Movimento');
+    SET in_AnoMes = JSON_VALUE(in_Arguments, '$.AnoMes');
+    
     SELECT
       CASE in_Visao
         WHEN "F" THEN JSON_VALUE(b.Extra, '$.DataFatura')
         WHEN "G" THEN a.Mes
-        WHEN "T" THEN JSON_VALUE(c.Extra, '$.DataPagamento')
-        END AS Dia,
-      CASE WHEN LEFT(in_Conta, 2) != "FO" THEN ACC_ExtrairConta("CR", a.Conta) END AS CentroResultados,
-      CASE WHEN LEFT(in_Conta, 2) != "FO" THEN ACC_ExtrairConta("AN", a.Conta) END AS Analitica,
+      END AS Dia,
+      ACC_ExtrairConta("CR", a.Conta) AS CentroResultados,
+      ACC_ExtrairConta("AN", a.Conta) AS Analitica,
       JSON_VALUE(b.Extra, '$.Descricao') AS Descricao,
       CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra)) AS DECIMAL(18, 2)) AS Valor,
       b.FileId
-    FROM <?=tableNameWithModule("Lancamentos")?> AS a
-      LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS b ON a.DocNumSerie = b.NumSerie
-      LEFT JOIN <?=tableNameWithModule("Documentos","DOC")?> AS c ON JSON_VALUE(b.Extra, '$.ComprovativoPagamentoId') = c.Id
-    WHERE a.Conta LIKE CONCAT("%", in_Conta, "%") AND
-      CASE WHEN LEFT(in_Conta, 2) = "FO" THEN a.CoefRateio > 0 ELSE 1 END
+    FROM ACC_Lancamentos AS a
+      LEFT JOIN DOC_Documentos AS b ON a.DocNumSerie = b.NumSerie
+    WHERE a.Conta LIKE CONCAT("%", in_Agregador, "%") AND
+      CASE in_Movimento
+        WHEN "C" THEN a.CoefRateio < 0
+        WHEN "P" THEN a.CoefRateio > 0
+      ELSE 1 END
     GROUP BY Dia, FileId, CentroResultados, Analitica
     HAVING EXTRACT(YEAR_MONTH FROM Dia) = in_AnoMes
     ORDER BY Dia;
+    
+    
+    -- -------
+    -- 2. Visão: T (Agregador: FOXXX / CLXXX, Movimento: -, AnoMes: -)
+    -- -------
+    ELSEIF in_Visao = "T" THEN
+    
+      SELECT
+        JSON_VALUE(b.Extra, '$.DataFatura') AS DataFatura,
+        JSON_VALUE(b.Extra, '$.Descricao') AS Descricao,
+        CAST(SUM(a.CoefRateio * ACC_FaturaValorTotal(b.Extra)) AS DECIMAL(18, 2)) AS Valor,
+        b.FileId
+      FROM ACC_Lancamentos AS a
+        LEFT JOIN DOC_Documentos AS b ON a.DocNumSerie = b.NumSerie
+      WHERE a.Conta = in_Agregador
+      GROUP BY a.Conta, a.DocNumSerie
+      HAVING Valor != 0
+      ORDER BY DataFatura;
+      
+    END IF;
     
   END;
   
